e1b9c8ef286bac9e2a40c8c40e2702557482e0d3
Albe MARINI-AUDOUARD <amarini-@e1r13p10.42.fr>
2022-07-03 23:05:19 +0200

    ✨💩 -started with classes, mainly comments and probably shit, will need to see tomorow what it entails and what to do

---
+++ module_00/ex01/phonebook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/03 22:06:59 by amarini-          #+#    #+#             */
+/*   Updated: 2022/07/03 23:03:46 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "contact.hpp"
+
+class phonebook
+{
+	public:
+
+	contact	contacts[8];
+	// add contacts ?
+	// constructor
+	// destructor
+	private:
+	// nbr of current contacts
+};
