139bb85c8b2a415fd1d2edd5a9685a4d304e6511
Albe MARINI-AUDOUARD <amarini-@e1r9p8.42.fr>
2022-07-04 13:34:55 +0200

    ✨ -Added contact variables to contact class

--- module_00/ex01/contact.hpp
+++ module_00/ex01/contact.hpp
@@ -6,16 +6,20 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 22:10:19 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/03 22:13:25 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:33:57 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-class	contact
+class	Contact
 {
 	public:
-	// first name
-	// last name
-	// mickname
-	// phone number
-	// darkest secret
+	// constructor
+	// destructor
+	char	*first_name;
+	char	*last_name;
+	char	*nickname;
+	int		phone_number;
+	char	*darkest_secret;
+	// private ?
+	int		index;
 };
