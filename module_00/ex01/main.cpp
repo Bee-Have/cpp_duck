53bd43686019ab0a93a714b13357054bc357cfb9
Albe MARINI-AUDOUARD <amarini-@e1r12p14.42.fr>
2022-07-05 14:13:40 +0200

    🚸 -Added usage in no correct commands are entered

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 13:36:32 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/05 13:33:00 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/05 14:13:02 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,15 @@ int	main(void)
 		std::getline(std::cin, line);
 		if (line.compare("EXIT") == 0)
 			break ;
-		if (line.compare("ADD") == 0)
+		else if (line.compare("ADD") == 0)
 		{
 			std::cout << "add contact" << std::endl;
 			phonebook.add_contact();
 		}
-		if (line.compare("SEARCH") == 0)
+		else if (line.compare("SEARCH") == 0)
 			phonebook.search();
+		else if (std::cin)
+			std::cout << "ADD: Add a contact | SEARCH: Search existing contacts | EXIT: Exits phonebook" << std::endl;
 	}
 	std::cout << "Your Phonebook has been deleted" << std::endl;
 	return (0);
