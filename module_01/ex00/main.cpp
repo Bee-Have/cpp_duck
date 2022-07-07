e4b4c6a82f870af32a80a327e884181ecda033ec
Albe MARINI-AUDOUARD <amarini-@e1r12p14.42.fr>
2022-07-07 12:57:36 +0200

    🎨🚚 🎨 -Formatted print in main so it's a little more readable (not perfect though) 🚚 -Rename zombie.cpp file to Zombie.cpp file as asked by the subject

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 13:11:26 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/06 15:24:15 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/07 12:45:13 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ int	main(void)
 
 	std::cout << RED <<" BZZZ.. START EXPERIMENT BZZZZZZZZ.\n\n" << END;
 
-	std::cout << YELLOW << " CR-CREATING" << END << CYAN << " STACK" << END << YELLOW << " BZZ Z-ZZOMBIE :\n" << END;
+	std::cout << YELLOW << " CR-CREATING" << END << CYAN << " STACK" << END
+		<< YELLOW << " BZZ Z-ZZOMBIE :\n" << END;
 	zombie_stack = newZombie(stack);
 	std::cout << CYAN << " STACK" << END << GREEN << " ZOMBIE CRRRREATED BZZZZ\n" << END;
 	
@@ -41,16 +42,22 @@ int	main(void)
 	zombie_stack->announce();
 	std::cout << GREEN << " SSUUUCCESSSSSSS\n" << END;
 
-	std::cout << YELLOW << " DELETING" << END << CYAN <<" S-SSSTACK"<< END << YELLOW <<" ZZZZOMBIIIIIIE :\n" << END;
+	std::cout << YELLOW << " DELETING" << END << CYAN <<" S-SSSTACK"<< END
+		<< YELLOW <<" ZZZZOMBIIIIIIE :\n" << END;
 	delete zombie_stack;
 	std::cout << CYAN << " STACK " << END << GREEN << "BZZ-BZZZZT Z-Z-ZOMBIE DELETEEEEED\n" << END;
 
-	std::cout << YELLOW <<"\n CRRRREATING BZZZ" << END << CYAN <<" HEAP" << END << YELLOW << " BZZZZZ-ZOMBIIIE :\n" << END;
+	std::cout << YELLOW <<"\n CRRRREATING BZZZ" << END << CYAN <<" HEAP" << END
+		<< YELLOW << " BZZZZZ-ZOMBIIIE :\n" << END;
 	randomChump(heap);
 	std::cout << CYAN << " HEAP" << END << GREEN << " Z-ZZZ-ZZZZOMBIE CR-CRRRRR-CREATED\n" << END;
-	std::cout << CYAN << " HEAP" << END << RED << " ZZ-ZZZOMBIE DIEDD.. FAILUUUUURE BZZZZ" << END << std::endl;
+	std::cout << CYAN << " HEAP" << END << RED << " ZZ-ZZZOMBIE DIEDD.. FAILUUUUURE BZZZZ"
+		<< END << std::endl;
 
 	std::cout << RED << "\n EXPERIMENT RESULT BZZZZ :\n" << END;
-	std::cout << YELLOW << " BZZZ" << END << CYAN << " STACK " << END << YELLOW << "ZOMBIE STAY ALIVE UNTIL DESTROYED\n" << END;
-	std::cout << CYAN << " HEAP " << END << YELLOW << "ZZZ-ZZOMBIE BZZZ DIES AS SOON AS BZZZ IT LEAVES FFFFUNCTION\n" << END;
+	std::cout << YELLOW << " BZZZ" << END << CYAN << " STACK " << END << YELLOW
+		<< "ZOMBIE STAY ALIVE UNTIL DESTROYED\n" << END;
+	std::cout << CYAN << " HEAP " << END << YELLOW
+		<< "ZZZ-ZZOMBIE BZZZ DIES AS SOON AS BZZZ IT LEAVES FFFFUNCTION\n" << END;
+	return (0);
 }
