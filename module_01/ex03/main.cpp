36cd2131d4690476e7ebf006b68222c6efeca136
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-11 17:31:16 +0200

    ✨ -Added main and makefile for the compilation and testing

---
+++ module_01/ex03/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 15:16:34 by amarini-          #+#    #+#             */
+/*   Updated: 2022/07/11 17:25:31 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "Weapon.hpp"
+#include <iostream>
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
