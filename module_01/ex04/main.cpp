bbaf77d9a3009ee2c37e4978abd74a784ac3408d
Albe MARINI-AUDOUARD <amarini-@e1r12p19.clusters.42paris.fr>
2022-07-12 21:02:49 +0200

    basically finished my exercie, i completely forgot to push, i'm dumb, sorry

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,19 +6,65 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 18:33:19 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/11 18:36:32 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/12 20:23:00 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <filesystem>
 
 int	main(int ac, char *av[])
 {
+	std::ifstream	infile;
+	std::ofstream	outfile;
 	if (ac != 4)
 	{
-		std::cerr << "Usage: ./replace filename string_1 string_2\n";
+		std::cerr << "Usage: ./replace [FILE] to_replace new_string\n";
 		return (1);
 	}
+	if (av[2][0] == '\0')
+	{
+		std::cerr << "String to replace cannot be empty\n";
+		return (1);
+	}
+	infile.open (av[1]);
+	if (infile.is_open())
+	{
+		std::string	newName;
+		newName.assign(av[1]);
+		newName.append(".replace");
+		outfile.open (newName.c_str());
+		if (outfile.is_open())
+		{
+			std::string	content;
+			std::string	line;
+			while (std::getline (infile, line))
+			{
+				if (content.empty() == 0)
+					content.append("\n");
+				content.append(line);
+			}
+			if (line.empty() == 1)
+				content.append("\n");
+			std::string	needle(av[2]);
+			std::string	newNeedle(av[3]);
+			std::size_t	found = 0;
+			while ((found = content.find(needle, found)) != std::string::npos)
+			{
+				content.erase(found, needle.size());
+				content.insert(found, newNeedle);
+				found += newNeedle.size();
+			}
+			outfile << content;
+			infile.close();
+			outfile.close();
+		}
+		else
+			std::cerr << "could not open : [" << newName << "]\n";
+	}
+	else
+		std::cerr << '[' << av[1] << "] can't be opened\n";
 	return (0);
 }
---
+++ module_01/ex04/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+make
+
+YELLOW="\e[33m"
+GREEN="\e[32m"
+END="\e[0m"
+
+echo "${YELLOW}\n REPLACING [duck] by [garbage] in [./text/simple.txt]${END}"
+./replace ./text/simple.txt duck garbage
+echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [ ] by [.] in [./text/simple.txt.replace]${END}"
+./replace ./text/simple.txt.replace " " "."
+echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [garbage] by [duck duck] in [./text/simple.txt.replace.replace]${END}"
+./replace ./text/simple.txt.replace.replace garbage duckduck
+echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace.replace.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [\ n] by [ ] in [./text/callofCthulhu.txt]${END}"
+./replace ./text/callofCthulhu.txt "\n" " "
+echo "${GREEN}RESULTS can be found in -> [./text/callofCthulhu.txt.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [,] by [then] in [./text/callofCthulhu.txt.replace]${END}"
+./replace ./text/callofCthulhu.txt.replace "," "then"
+echo "${GREEN}RESULTS can be found in -> [./text/callofCthulhu.txt.replace.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [Lorem ipsum dolor sit amet] by [random gibberish] in [./text/loremIpsum.txt]${END}"
+./replace ./text/loremIpsum.txt "Lorem ipsum dolor sit amet" "random gibberish"
+echo "${GREEN}RESULTS can be found in -> [./text/loremIpsum.txt.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [] by [] in [./text/loremIpsum.txt.replace]${END}"
+./replace ./text/loremIpsum.txt.replace "" ""
+echo "${GREEN}RESULTS can be found in -> [./text/loremIpsum.txt.replace.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [ ] by [.] in [./text/Nightmare.txt]${END}"
+./replace ./text/Nightmare.txt " " "."
+echo "${GREEN}RESULTS can be found in -> [./text/Nightmare.txt.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [Hello there General Kenobi] by [] in [./text/oneSentence.txt]${END}"
+./replace ./text/oneSentence.txt "Hello there General Kenobi" ""
+echo "${GREEN}RESULTS can be found in -> [./text/oneSentence.txt.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [] by [HEY THERE *wink*] in [./text/oneSentence.txt.replace]${END}"
+./replace ./text/oneSentence.txt.replace "" "HEY THERE *wink*"
+echo "${GREEN}RESULTS can be found in -> [./text/oneSentence.txt.replace.replace]${END}"
+
+echo "${YELLOW}\n REPLACING [whole text] by [fearlesspasteque] in [./text/Nightmare.txt.replace]${END}"
+./replace ./text/Nightmare.txt.replace "$(cat ./text/Nightmare.txt.replace)" "fearlesspasteque"
+echo "${GREEN}RESULTS can be found in -> [./text/Nightmare.txt.replace.replace]${END}"
+
