e7884e367f131b84705054a9bb9980941e4b44db
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-16 22:18:22 +0200

    💡 -Misspeled "scope" in one of my explanatory comments

--- module_01/ex00/randomChump.cpp
+++ module_01/ex00/randomChump.cpp
@@ -6,13 +6,13 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 12:44:15 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/06 13:07:06 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:16:11 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-// here the zombie will only exist in the score of the function.
+// here the zombie will only exist in the scope of the function.
 // the moment the function is over, the zombie is destroyed automatically
 void	randomChump(std::string name)
 {
