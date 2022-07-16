8a33e47c092769ab329c5b93a8c975814b427f98
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-16 22:47:52 +0200

    🐛 -Fixed memory leak. I was not exiting upon finding the error of 0 malloc in the ZombieHorde(). I am now properly exiting

--- module_01/ex01/zombieHorde.cpp
+++ module_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:50:51 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/16 22:37:26 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:47:18 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ Zombie	* zombieHorde(int N, std::string name)
 	Zombie	* Horde;
 
 	if (N <= 0)
+	{
 		std::cout << "zzzero or lezz iz brainzzz" << std::endl;
+		return (NULL);
+	}
 	Horde = new Zombie[N];
 	for (int i = 0; i < N; ++i)
 	{
