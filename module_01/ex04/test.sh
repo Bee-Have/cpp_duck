287d1d6d99c387fcc82738d267dea56f9387e37d
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-17 00:34:54 +0200

    ✅ -Added possibility of checking leaks inside the test.sh with argument given with "sh test.sh"

--- module_01/ex04/test.sh
+++ module_01/ex04/test.sh
@@ -6,47 +6,51 @@ YELLOW="\e[33m"
 GREEN="\e[32m"
 END="\e[0m"
 
+if [ $1 -eq 1 ]; then
+	export LEAK="valgrind --leak-check=full --show-leak-kinds=all"
+fi
+
 echo "${YELLOW}\n REPLACING [.] by [ duck] in [./text/simple.txt]${END}"
-./replace ./text/simple.txt . " duck"
+$LEAK ./replace ./text/simple.txt . " duck"
 echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [ ] by [.] in [./text/simple.txt.replace]${END}"
-./replace ./text/simple.txt.replace " " "."
+$LEAK ./replace ./text/simple.txt.replace " " "."
 echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [duck] by [duckduck] in [./text/simple.txt.replace.replace]${END}"
-./replace ./text/simple.txt.replace.replace duck duckduck
+$LEAK ./replace ./text/simple.txt.replace.replace duck duckduck
 echo "${GREEN}RESULTS can be found in -> [./text/simple.txt.replace.replace.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [\\\n] by [ ] in [./text/callOfCthulhu.txt]${END}"
-./replace ./text/callOfCthulhu.txt "\n" " "
+$LEAK ./replace ./text/callOfCthulhu.txt "
+" " "
 echo "${GREEN}RESULTS can be found in -> [./text/callOfCthulhu.txt.replace]${END}"
 
-echo "${YELLOW}\n REPLACING [,] by [then] in [./text/callOfCthulhu.txt.replace]${END}"
-./replace ./text/callOfCthulhu.txt.replace "," "then"
+echo "${YELLOW}\n REPLACING [,] by [VIRGULE] in [./text/callOfCthulhu.txt.replace]${END}"
+$LEAK ./replace ./text/callOfCthulhu.txt.replace "," "VIRGULE"
 echo "${GREEN}RESULTS can be found in -> [./text/callOfCthulhu.txt.replace.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [Lorem ipsum dolor sit amet] by [random gibberish] in [./text/loremIpsum.txt]${END}"
-./replace ./text/loremIpsum.txt "Lorem ipsum dolor sit amet" "random gibberish"
+$LEAK ./replace ./text/loremIpsum.txt "Lorem ipsum dolor sit amet" "random gibberish"
 echo "${GREEN}RESULTS can be found in -> [./text/loremIpsum.txt.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [] by [] in [./text/loremIpsum.txt.replace]${END}"
-./replace ./text/loremIpsum.txt.replace "" ""
+$LEAK ./replace ./text/loremIpsum.txt.replace "" ""
 echo "${GREEN}RESULTS can be found in -> [./text/loremIpsum.txt.replace.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [ ] by [.] in [./text/Nightmare.txt]${END}"
-./replace ./text/Nightmare.txt " " "."
+$LEAK ./replace ./text/Nightmare.txt " " "."
 echo "${GREEN}RESULTS can be found in -> [./text/Nightmare.txt.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [Hello there General Kenobi] by [] in [./text/oneSentence.txt]${END}"
-./replace ./text/oneSentence.txt "Hello there General Kenobi" ""
+$LEAK ./replace ./text/oneSentence.txt "Hello there General Kenobi" ""
 echo "${GREEN}RESULTS can be found in -> [./text/oneSentence.txt.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [] by [HEY THERE *wink*] in [./text/oneSentence.txt.replace]${END}"
-./replace ./text/oneSentence.txt.replace "" "HEY THERE *wink*"
+$LEAK ./replace ./text/oneSentence.txt.replace "" "HEY THERE *wink*"
 echo "${GREEN}RESULTS can be found in -> [./text/oneSentence.txt.replace.replace]${END}"
 
 echo "${YELLOW}\n REPLACING [whole text] by [fearlesspasteque] in [./text/Nightmare.txt.replace]${END}"
-./replace ./text/Nightmare.txt.replace "$(cat ./text/Nightmare.txt.replace)" "fearlesspasteque"
+$LEAK ./replace ./text/Nightmare.txt.replace "$(cat ./text/Nightmare.txt.replace)" "fearlesspasteque"
 echo "${GREEN}RESULTS can be found in -> [./text/Nightmare.txt.replace.replace]${END}"
-
