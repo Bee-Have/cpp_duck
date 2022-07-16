15843daee3049bebd0057cffa791f2481e1e1cde
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-16 22:35:48 +0200

    🔥🚸 -Deleted useless END of colors in my prints, only one is needed, i didn't check :p

--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 13:11:26 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/07 12:45:13 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:34:27 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,33 +31,32 @@ int	main(void)
 
 	std::cout << RED <<" BZZZ.. START EXPERIMENT BZZZZZZZZ.\n\n" << END;
 
-	std::cout << YELLOW << " CR-CREATING" << END << CYAN << " STACK" << END
-		<< YELLOW << " BZZ Z-ZZOMBIE :\n" << END;
+	std::cout << YELLOW << " CR-CREATING"<< CYAN << " STACK" << YELLOW
+		<< " BZZ Z-ZZOMBIE :\n" << END;
 	zombie_stack = newZombie(stack);
-	std::cout << CYAN << " STACK" << END << GREEN << " ZOMBIE CRRRREATED BZZZZ\n" << END;
+	std::cout << CYAN << " STACK" << GREEN << " ZOMBIE CRRRREATED BZZZZ\n" << END;
 	
-	std::cout << CYAN << " STACK" << END << YELLOW << " ZZ-ZOMBIE... SPEEEEEEAK :\n" << END;
+	std::cout << CYAN << " STACK" << YELLOW << " ZZ-ZOMBIE... SPEEEEEEAK :\n" << END;
 	zombie_stack->announce();
 	std::cout << YELLOW << " A-A-AGAINN :\n" << END;
 	zombie_stack->announce();
 	std::cout << GREEN << " SSUUUCCESSSSSSS\n" << END;
 
-	std::cout << YELLOW << " DELETING" << END << CYAN <<" S-SSSTACK"<< END
-		<< YELLOW <<" ZZZZOMBIIIIIIE :\n" << END;
+	std::cout << YELLOW << " DELETING" << CYAN <<" S-SSSTACK" << YELLOW
+		<<" ZZZZOMBIIIIIIE :\n" << END;
 	delete zombie_stack;
-	std::cout << CYAN << " STACK " << END << GREEN << "BZZ-BZZZZT Z-Z-ZOMBIE DELETEEEEED\n" << END;
+	std::cout << CYAN << " STACK " << GREEN << "BZZ-BZZZZT Z-Z-ZOMBIE DELETEEEEED\n" << END;
 
-	std::cout << YELLOW <<"\n CRRRREATING BZZZ" << END << CYAN <<" HEAP" << END
-		<< YELLOW << " BZZZZZ-ZOMBIIIE :\n" << END;
+	std::cout << YELLOW <<"\n CRRRREATING BZZZ" << CYAN <<" HEAP" << YELLOW
+		<< " BZZZZZ-ZOMBIIIE :\n" << END;
 	randomChump(heap);
-	std::cout << CYAN << " HEAP" << END << GREEN << " Z-ZZZ-ZZZZOMBIE CR-CRRRRR-CREATED\n" << END;
-	std::cout << CYAN << " HEAP" << END << RED << " ZZ-ZZZOMBIE DIEDD.. FAILUUUUURE BZZZZ"
-		<< END << std::endl;
+	std::cout << CYAN << " HEAP" << GREEN << " Z-ZZZ-ZZZZOMBIE CR-CRRRRR-CREATED\n" << END;
+	std::cout << CYAN << " HEAP" << RED << " ZZ-ZZZOMBIE DIEDD.. FAILUUUUURE BZZZZ" << END << std::endl;
 
 	std::cout << RED << "\n EXPERIMENT RESULT BZZZZ :\n" << END;
-	std::cout << YELLOW << " BZZZ" << END << CYAN << " STACK " << END << YELLOW
+	std::cout << YELLOW << " BZZZ" << CYAN << " STACK " << YELLOW 
 		<< "ZOMBIE STAY ALIVE UNTIL DESTROYED\n" << END;
-	std::cout << CYAN << " HEAP " << END << YELLOW
+	std::cout << CYAN << " HEAP " << YELLOW
 		<< "ZZZ-ZZOMBIE BZZZ DIES AS SOON AS BZZZ IT LEAVES FFFFUNCTION\n" << END;
 	return (0);
 }
--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 12:16:08 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/07 13:49:15 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:31:10 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	main(void)
 	std::cout << RED <<" BZZZ.. START EXPERIMENT BZZZZZZZZ.\n" << END;
 
 	// create stack of 10
-	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << END << CYAN
-		<< "T-TEN" << END << YELLOW << " : \n" << END;
+	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << CYAN
+		<< "T-TEN" << YELLOW << " : \n" << END;
 	Horde = zombieHorde(10, "Alpha");
 	std::cout << YELLOW << " SP-SPEEEEAAAK\n" << END;
 	for(int i = 0; i < 10; ++i)
@@ -36,14 +36,14 @@ int	main(void)
 		std::cout << GREEN << "INDEX [" << Horde[i].get_index() << "] " << END;
 		Horde[i].announce();
 	}
-	std::cout << YELLOW << " DE-DELETING HORDE OF " << END << CYAN << "T-TEN"
-		<< END << YELLOW << " :\n" << END;
+	std::cout << YELLOW << " DE-DELETING HORDE OF " << CYAN << "T-TEN"
+		<< YELLOW << " :\n" << END;
 	delete[] Horde;
 	std::cout  << GREEN << " BZZZZZZZZZ... SUCCESSSSSSSSSSS\n" << END;
 
 	// create stack of max 20
-	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << END << CYAN
-		<< " TWENNNTY" << END << YELLOW << " : \n" << END;
+	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << CYAN
+		<< " TWENNNTY" << YELLOW << " : \n" << END;
 	Horde = zombieHorde(20, "Beta");
 	std::cout << YELLOW << " SP-SPEEEEAAAK\n" << END;
 	for(int i = 0; i < 20; ++i)
@@ -51,25 +51,25 @@ int	main(void)
 		std::cout << GREEN << "INDEX [" << Horde[i].get_index() << "] " << END;
 		Horde[i].announce();
 	}
-	std::cout << YELLOW << " DE-DELETING HORDE OF " << END << CYAN << "TWENNNTY"
-		<< END << YELLOW << " :\n" << END;
+	std::cout << YELLOW << " DE-DELETING HORDE OF " << CYAN << "TWENNNTY"
+		<< YELLOW << " :\n" << END;
 	delete[] Horde;
 	std::cout  << GREEN << " BZZZZZZZZZ... SUCCESSSSSSSSSSS\n" << END;
 
 	// create stack of 0
-	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << END << CYAN
-		<< " ZERO " << END << YELLOW << ":\n" << END;
+	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << CYAN
+		<< " ZERO " << YELLOW << ":\n" << END;
 	Horde = zombieHorde(0, "Gamma");
 	std::cout << RED << "BZZ FORBIDDEN... BZZZZZ FAILUUUUURE\n" << END;
 
 	// create stack of 1
-	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << END << CYAN
-		<< " ONE" << END << YELLOW << " :\n";
+	std::cout << YELLOW << "\n CR-CREATTTING HORDE OF BZZZZZ" << CYAN
+		<< " ONE" << YELLOW << " :\n" << END;
 	Horde = zombieHorde(1, "Delta");
 	std::cout << YELLOW << " SP-SPEEEEAAAK\n" << END;
 	Horde[0].announce();
-	std::cout << YELLOW << "\n DE-DELETING HORDE OF " << END << CYAN << "ONE"
-		<< END << YELLOW << " :\n" << END;
+	std::cout << YELLOW << "\n DE-DELETING HORDE OF " << CYAN << "ONE"
+		<< YELLOW << " :\n" << END;
 	delete[] Horde;
 	std::cout  << GREEN << " BZZZZZZZZZ... SUCCESSSSSSSSSSS\n" << END;
 
