21bd6429abc1fc58a409da265cd487c58e92033c
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-07-16 22:37:46 +0200

    🚸🩹 -Deletd colors on the error for the creation of the zombie horde, it looked weird compared to the other messages so i took the color off

--- module_01/ex01/zombieHorde.cpp
+++ module_01/ex01/zombieHorde.cpp
@@ -6,21 +6,18 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:50:51 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/07 13:38:02 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:37:26 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-
-#define RED "\033[1;31m"
-#define END "\33[0m"
 
 Zombie	* zombieHorde(int N, std::string name)
 {
 	Zombie	* Horde;
 
 	if (N <= 0)
-		std::cout << RED << "zzzero or lezz iz brainzzz" << END << std::endl;
+		std::cout << "zzzero or lezz iz brainzzz" << std::endl;
 	Horde = new Zombie[N];
 	for (int i = 0; i < N; ++i)
 	{
