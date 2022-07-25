df3f4b3faf98ec7b0238e0df98d53ec01b958f9c
Albe MARINI-AUDOUARD <amarini-@e1r11p8.clusters.42paris.fr>
2022-07-25 16:58:14 +0200

    ✨ -Added module_03 directory as well as the first two files which are not yet fully initiaized

---
+++ module_03/ClapTrap.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 16:39:02 by amarini-          #+#    #+#             */
+/*   Updated: 2022/07/25 16:58:03 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+// Constructors :
+ClapTrap::ClapTrap()
+{
+	
+}
+
+ClapTrap::ClapTrap(ClapTrap &cpy)
+{
+	
+}
+
+// Destructor :
+ClapTrap::~ClapTrap()
+{
+	
+}
+
+// Assignment operator :
+ClapTrap	&ClapTrap::operator=(const ClapTrap &assign)
+{
+	
+}
+
+// Internal methods :
+void	ClapTrap::attack(const std::string& traget)
+{
+	
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	
+}
+
+// Accessors :
+std::string	ClapTrap::get_name(void) const
+{
+	
+}
+
+void		ClapTrap::set_name(std::string &new_name)
+{
+	
+}
+
+int			ClapTrap::get_hitpts(void) const
+{
+	
+}
+
+void		ClapTrap::set_hitpts(int &new_hitpts)
+{
+	
+}
+
+int			ClapTrap::get_energypts(void) const
+{
+	
+}
+
+void		ClapTrap::set_energypts(int &new_energypts)
+{
+	
+}
+
+int			ClapTrap::get_attackdmg(void) const
+{
+	
+}
+
+void		ClapTrap::set_attackdmg(int &new_attackdmg)
+{
+	
+}
---
+++ module_03/ClapTrap.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 16:57:58 by amarini-          #+#    #+#             */
+/*   Updated: 2022/07/25 16:58:00 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class ClapTrap
+{
+	public:
+	// Constructors :
+		ClapTrap();
+		ClapTrap(ClapTrap &cpy);
+	// Destructor :
+		~ClapTrap();
+	// Assignment operator (=) :
+		ClapTrap	&operator=(const ClapTrap &assign);
+
+	// Member functions :
+		void	attack(const std::string& traget);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+	// Accessors :
+		std::string	get_name(void) const;
+		void		set_name(std::string &new_name);
+		int			get_hitpts(void) const;
+		void		set_hitpts(int &new_hitpts);
+		int			get_energypts(void) const;
+		void		set_energypts(int &new_energypts);
+		int			get_attackdmg(void) const;
+		void		set_attackdmg(int &new_attackdmg);
+
+	private:
+		std::string	_name;
+		int			_hit_pts;
+		int			_energy_pts;
+		int			_attack_dmg;
+};
