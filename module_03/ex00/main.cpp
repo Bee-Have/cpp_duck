b961a9eeec8583f3525a718859c4f69c2d61be59
Albe MARINI-AUDOUARD <amarini-@e2r1p6.clusters.42paris.fr>
2022-07-26 10:46:48 +0200

    ✅ -Added tests to the main

--- module_03/ex00/main.cpp
+++ module_03/ex00/main.cpp
@@ -6,35 +6,42 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 19:19:31 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/25 20:01:47 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/26 10:44:05 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "ClapTrap.hpp"
 
+#define RED "\033[1;31m"
+#define END "\33[0m"
+
 int	main(void)
 {
-	ClapTrap	Charles("Charles");
-	ClapTrap	Lucas("Lucas");
-	ClapTrap	Unknown;
+	ClapTrap	Charles("chdespon");
+	ClapTrap	Lucas("ldutriez");
+	ClapTrap	Unknown("Random Person");
 	
-	std::cout << "Basic tests :\n";
+	std::cout << RED << "\nBasic tests :\n" << END;
 	{
 		Lucas.set_hitpts(50);
+		Charles.set_attackdmg(3);
 		Charles.attack(Lucas.get_name());
 		Lucas.takeDamage(Charles.get_attackdmg());
 		Charles.set_attackdmg(100);
 		Charles.attack(Lucas.get_name());
 		Lucas.takeDamage(Charles.get_attackdmg());
 	}
-	std::cout << "\nError tests :\n";
+	std::cout << RED << "\nError tests :\n" << END;
 	{
 		Charles.set_hitpts(0);
 		Lucas.set_attackdmg(1);
 		Lucas.attack(Charles.get_name());
 		Charles.takeDamage(Lucas.get_attackdmg());
+		Charles.attack(Lucas.get_name());
 		Lucas.set_energypts(0);
+		Lucas.attack(Charles.get_name());
+		Unknown.set_name("");
 	}
 
 	return (0);
