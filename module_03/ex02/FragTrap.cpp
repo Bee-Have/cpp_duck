86f8ac25a1cf0ef5660cf8924e7cfe691299b86f
Albe MARINI-AUDOUARD <amarini-@e2r12p6.clusters.42paris.fr>
2022-07-27 12:35:17 +0200

    ✨ -Finished setting up FragTrap class and it's methods. Nothing is specified for the messages of the methods so i did not change them

--- module_03/ex02/FragTrap.cpp
+++ module_03/ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 19:03:03 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/26 19:09:32 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/27 12:35:00 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,36 +24,44 @@ FragTrap::FragTrap(void): ClapTrap()
 	_attack_dmg = 30;
 }
 
-FragTrap::FragTrap(const char *name)
+FragTrap::FragTrap(const char *name): ClapTrap(name)
 {
-	
+	std::cout << "It is I " << _name << std::endl;
+	_hit_pts = 100;
+	_energy_pts = 100;
+	_attack_dmg = 30;
 }
 
-FragTrap::FragTrap(FragTrap &newfrag)
-{
-	
-}
+FragTrap::FragTrap(FragTrap &newfrag): ClapTrap(newfrag)
+{}
 
 /**
 ------------------------------| DESTRUCTOR |------------------------------------
 **/
 FragTrap::~FragTrap(void)
 {
-
+	std::cout << _name << " out !" << std::endl;
 }
 
 /**
 --------------------------| ASSIGNMENT OPERATOR |-------------------------------
 **/
-FragTrap	&FragTrap::operator=(const FragTrap &comp)
+FragTrap	&FragTrap::operator=(const FragTrap &newfrag)
 {
-	
+	if (this != &newfrag)
+	{
+		this->_name.assign(newfrag._name);
+		this->_hit_pts = newfrag._hit_pts;
+		this->_energy_pts = newfrag._energy_pts;
+		this->_attack_dmg = newfrag._attack_dmg;
+	}
+	return (*this);
 }
 
 /**
 --------------------------------| METHODS |-------------------------------------
 **/
-void	FragTrap::highFivesGuys()
+void	FragTrap::highFivesGuys(void)
 {
-	
+	std::cout << _name << " is requesting the HIGH FIVE" << std::endl;
 }
--- module_03/ex02/FragTrap.hpp
+++ module_03/ex02/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 18:55:51 by amarini-          #+#    #+#             */
-/*   Updated: 2022/07/26 19:02:50 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/07/27 12:32:37 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class FragTrap : public ClapTrap
 	// Destructor
 		~FragTrap();
 	// Operator
-		FragTrap	&operator=(const FragTrap &comp);
+		FragTrap	&operator=(const FragTrap &newfrag);
 	
 	// Methods
 		void	highFivesGuys();
