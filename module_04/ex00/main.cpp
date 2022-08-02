0149f646757427d7c6cd728a86463870961bb24c
Albe MARINI-AUDOUARD <amarini-@e1r1p12.clusters.42paris.fr>
2022-08-02 19:47:35 +0200

    ✨🙈 ✨ -Added main.cpp to be able to add tests ✨ -Added Makefile to be able to actually test everything 🙈 -Added gitignore file to not accidentally push .o and binaries

---
+++ module_04/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 19:35:08 by amarini-          #+#    #+#             */
+/*   Updated: 2022/08/02 19:37:49 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	return (0);
+}
