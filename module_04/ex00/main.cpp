47e6018e9bfe7bfa07d54f1a1f8075972e25ecbb
Albe MARINI-AUDOUARD <amarini-@e1r9p19.clusters.42paris.fr>
2022-08-16 07:36:07 +0200

    ✨ -Added WrongAnimals to main and tests to prove the change of behavior, also added them to the Makefile

--- module_04/ex00/main.cpp
+++ module_04/ex00/main.cpp
@@ -6,21 +6,25 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 19:35:08 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/16 01:39:44 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/16 07:35:40 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+
+#include "WrongAnimal.hpp"
+#include "WrongDog.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
 	std::cout << "Mandatory Polymorphism tests :\n";
 	{
 		std::cout << "	Constructors : \n";
-
 		const Animal* meta = new Animal();
 		const Animal* j = new Dog();
 		const Animal* i = new Cat();
@@ -33,29 +37,26 @@ int main()
 		meta->makeSound();
 
 		std::cout << "\n	Destructors : \n";
-
 		delete meta;
 		delete j;
 		delete i;
 	}
 	
-	std::cout << "Counter NON-Polymorphism tests :\n";
+	std::cout << "\nCounter NON-Polymorphism tests :\n";
 	{
 		std::cout << "	Constructors : \n";
-
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
+		const WrongAnimal* meta = new WrongAnimal();
+		const WrongAnimal* j = new WrongDog();
+		const WrongAnimal* i = new WrongCat();
 
 		std::cout << "\n	Tests : \n";
 		std::cout << j->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
+		i->makeSound(); //will output the WrongAnimal sound!
 		j->makeSound();
 		meta->makeSound();
 
 		std::cout << "\n	Destructors : \n";
-
 		delete meta;
 		delete j;
 		delete i;
