a81f33a2613f9b56a31de980378840c11c7a6915
Albe MARINI-AUDOUARD <amarini-@e1r10p11.clusters.42paris.fr>
2022-08-24 19:38:53 +0200

    ✅ -Added working tests to better show polymophism in main, the previous tests were not actually working

--- module_04/ex00/srcs/main.cpp
+++ module_04/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 19:35:08 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/24 16:21:07 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/24 19:26:34 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,55 +30,76 @@ int main()
 {
 	std::cout << GB << "Mandatory Polymorphism tests :\n" << END;
 	{
-		std::cout << YB << "	Constructors : \n" << END;
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
+		std::cout << YB << "Constructors : \n" << END;
+		const Animal* base = new Animal();
+		const Animal* derived_cat = new Cat();
+		const Animal* derived_dog = new Dog();
 
-		std::cout << YB << "	Tests : \n" << END;
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
-		j->makeSound();
-		meta->makeSound();
+		std::cout << YB << "Tests : \n" << END;
+		std::cout << "type: " << base->getType() << "\n";
+		std::cout << "type: " << derived_cat->getType() << "\n";
+		std::cout << "type: " << derived_dog->getType() << "\n";
+		base->makeSound(); // will output the animal/base sound!
+		derived_cat->makeSound(); // will output the cat/derived sound!
+		derived_dog->makeSound(); // will outpit the dog/derived sound!
 
-		std::cout << YB << "	Destructors : \n" << END;
-		delete meta;
-		delete j;
-		delete i;
+		std::cout << YB << "Destructors : \n" << END;
+		delete base;
+		delete derived_cat;
+		delete derived_dog;
 	}
 
 	std::cout << GB << "\nCounter NON-Polymorphism tests :\n" << END;
 	{
-		std::cout << YB << "	Constructors : \n" << END;
-		const WrongAnimal* meta = new WrongAnimal();
-		const WrongAnimal* j = new WrongDog();
-		const WrongAnimal* i = new WrongCat();
+		std::cout << YB << "Constructors : \n" << END;
+		const WrongAnimal* base = new WrongAnimal();
+		const WrongAnimal* derived_cat = new WrongCat();
+		const WrongAnimal* derived_dog = new WrongDog();
 
-		std::cout << YB << "	Tests : \n" << END;
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the WrongAnimal sound!
-		j->makeSound();
-		meta->makeSound();
+		std::cout << YB << "Tests : \n" << END;
+		std::cout << "type: " << base->getType() << "\n";
+		std::cout << "type: " << derived_cat->getType() << "\n";
+		std::cout << "type: " << derived_dog->getType() << "\n";
+		base->makeSound(); // will output the animal/base sound!
+		derived_cat->makeSound(); // will output the animal/base sound!
+		derived_dog->makeSound(); // will outpit the animal/base sound!
 
-		std::cout << YB << "	Destructors : \n" << END;
-		delete meta;
-		delete j;
-		delete i;
+		std::cout << YB << "Destructors : \n" << END;
+		delete base;
+		delete derived_cat;
+		delete derived_dog;
 	}
 
-	std::cout << GB << "\nMy tests :\n" << END;
+	std::cout << GB << "\nNo allocation tests :\n" << END;
 	{
-		const Cat		cat_base;
-		const Animal	cat_derived(cat_base);
+		std::cout << YB << "Declaration of base and derived class objects :\n" << END;
+		Animal	base;
+		Cat		derived;
 
-		// dog_derived = &dog_base;
-		
-		std::cout << "base class type : " << cat_base.getType() << "\n";
-		std::cout << "derived class type : " << cat_derived.getType() << "\n";
-		cat_base.makeSound(); //will output Cat class sound
-		cat_derived.makeSound(); //will output Animal sound with Cat type
+		std::cout << YB << "\nTest with references :\n" << END;
+		Animal	&base_ref = base;
+		Animal	&derived_ref = derived;
+		std::cout << "base class, exemple :\n";
+		base_ref.makeSound();
+		std::cout << "derived class, polymorphism: \n";
+		derived_ref.makeSound();
+
+		std::cout << YB << "\nTest with pointers :\n" << END;
+		Animal	*base_point = &base;
+		Animal	*derived_point = &derived;
+		std::cout << "base class, exemple :\n";
+		base_point->makeSound();
+		std::cout << "derived class, polymorphism: \n";
+		derived_point->makeSound();
+
+		std::cout << "\nSpecification of type during function call :\n";
+		std::cout << "(both will display parent function message voluntarily)\n";
+		std::cout << "base class, exemple :\n";
+		base_ref.Animal::makeSound();
+		std::cout << "derived class, polymorphism :\n";
+		derived_ref.Animal::makeSound();
+
+		std::cout << YB << "Destructors : \n" << END;
 	}
 	
 	return (0);
