21c4e008ebbe7b92bf8dbb2cb5c7b5a700202eda
Albe MARINI-AUDOUARD <amarini-@e1r12p1.clusters.42paris.fr>
2022-08-26 08:29:56 +0200

    ✅ -implemented tests from previous exercices (need to implement new ones)

--- module_04/ex02/srcs/main.cpp
+++ module_04/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 19:35:08 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/26 07:17:38 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/26 08:14:15 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,55 @@
 
 int main()
 {
-	
+	std::cout << GB << "Simple test :\n" << END;
+	{
+		std::cout << YB << "Contructors :\n" << END;
+		// const Animal	*base = new Animal(); // this will not compile
+		const Animal	*derived_cat = new Cat();
+		const Animal	*derived_dog = new Dog();
+		
+		std::cout << YB << "Tests :\n" << END;
+		std::cout << "type: " << derived_cat->getType() << "\n";
+		std::cout << "type: " << derived_dog->getType() << "\n";
+		derived_cat->makeSound();
+		derived_dog->makeSound();
+
+		std::cout << YB << "Destructors :\n" << END;
+		delete derived_cat;
+		delete derived_dog;
+	}
+
+	std::cout << GB << "\nNo allocation tests :\n" << END;
+	{
+		std::cout << YB << "Constructor :\n" << END;
+		// Animal	test; // this will not compile
+		Cat		derived;
+
+		std::cout << YB << "Reference to derived classs test :\n" << END;
+		Animal	&reference = derived;
+		reference.makeSound();
+
+		std::cout << YB << "Pointer to derived class test :\n" << END;
+		Animal	*pointer = &derived;
+		pointer->makeSound();
+
+		// derived.Animal::makeSound(); // this will not compile
+	}
+
+	std::cout << GB << "\nSimple array tests :\n" << END;
+	{
+		// this poses no problem because no constructor i called
+		Animal	*tiny_hoard[2];
+		std::cout << YB << "Constructors :\n" << END;
+		tiny_hoard[0] = new Dog();
+		tiny_hoard[1] = new Cat();
+
+		std::cout << YB << "Tests :\n" << END;
+		tiny_hoard[0]->makeSound();
+		tiny_hoard[1]->makeSound();
+
+		std::cout << YB << "Destructors :\n" << END;
+		delete tiny_hoard[0];
+		delete tiny_hoard[1];
+	}
 }
