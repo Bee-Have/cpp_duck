66537538d7a5f0088a2fb1b0a51c409cf7432905
Albe MARINI-AUDOUARD <amarini-@e1r9p20.clusters.42paris.fr>
2022-08-28 01:44:28 +0200

    🚚✨ 🚚 -Moved Bureaucrat class files to the ex00 directory and into a new src directory ✨ -Added an exception class inside the bureaucrat class (a nested class)

--- module_05/ex00/src/Bureaucrat.hpp
+++ module_05/ex00/src/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:15:36 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/26 12:25:55 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/28 01:43:05 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,24 @@ class Bureaucrat
 	// Attributes
 		const std::string	name;
 		int					grade;
+
+	// Nested classes
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				char	*what()
+				{
+					return ("Error: Grade cannot be higher then 1\n");
+				}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				char	*what()
+				{
+					return ("Error: Grade cannot be lower then 150\n");
+				}
+		};
 };
 // Operator overload "<<"
 	std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj);
