36b7c0eda4cd6d2c2b81f830755a67daf139b5b0
Albe MARINI-AUDOUARD <amarini-@e1r9p20.clusters.42paris.fr>
2022-08-28 03:49:30 +0200

    ✨ -Added src files from ex00

---
+++ module_05/ex01/src/Bureaucrat.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 11:15:36 by amarini-          #+#    #+#             */
+/*   Updated: 2022/08/28 02:55:28 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+	public:
+	// Constructors
+		Bureaucrat();
+		Bureaucrat(Bureaucrat &cpy);
+		Bureaucrat(int grade);
+		Bureaucrat(int grade, const char *newname);
+	// Destructor
+		~Bureaucrat();
+	// Assignment opertor
+		Bureaucrat	&operator=(const Bureaucrat &assign);
+
+	// Accessors
+		std::string	getName() const;
+		int			getGrade() const;
+
+	// Methods
+		void	increment_grade();
+		void	decrement_grade();
+
+	// Nested classes
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char	*what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW
+				{
+					return ("Error: Grade cannot be higher then 1\n");
+				}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char	*what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW
+				{
+					return ("Error: Grade cannot be lower then 150\n");
+				}
+		};
+
+	private:
+	// Attributes
+		const std::string	name;
+		int					grade;
+};
+// Operator overload "<<"
+	std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj);
---
+++ module_05/ex01/src/main.cpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 01:57:07 by amarini-          #+#    #+#             */
+/*   Updated: 2022/08/28 03:25:52 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+#define GB "\033[1;32m"
+#define YB "\033[1;33m"
+#define END "\33[0m"
+
+int	main()
+{
+	std::cout << GB << "Simple tests :\n" << END;
+	{
+		std::cout << YB << "Bureaucrat Moss(10, Moss)\n" << END;
+		std::cout << YB << "Bureaucrat Jen(Moss)\n" << END;
+		Bureaucrat	Moss(10, "Moss");
+		Bureaucrat	Jen(Moss);
+
+		std::cout << Moss << "\n";
+		std::cout << Jen << "\n";
+
+		std::cout << YB << "Grade change test :\n" << END;
+		Moss.decrement_grade();
+		Jen.increment_grade();
+		std::cout << Moss << "\n";
+		std::cout << Jen << "\n";
+	}
+
+	std::cout << GB << "\nBad Constructor tests :\n" << END;
+	{
+		std::cout << YB << "Test with default constructor :\n" << END;
+		try
+		{
+			Bureaucrat	Moss;
+		}
+		catch (Bureaucrat::GradeTooLowException exception)
+		{
+			std::cout << exception.what();
+		}
+		std::cout << YB << "Test with int Constructor < 1 :\n" << END;
+		try
+		{
+			Bureaucrat	Roy(-19);
+		}
+		catch (Bureaucrat::GradeTooHighException exception)
+		{
+			std::cout << exception.what();
+		}
+		std::cout << YB << "Test with (int, name) Constructor > 150 :\n" << END;
+		try
+		{
+			Bureaucrat Jen(180, "Jen");
+		}
+		catch (Bureaucrat::GradeTooLowException exception)
+		{
+			std::cout << exception.what();
+		}
+	}
+
+	std::cout << GB << "\nTest incrementing/decrementing maximum grades :\n" << END;
+	{
+		Bureaucrat	Jen(1, "Jen");
+		Bureaucrat	Roy(150, "Roy");
+
+		std::cout << Jen << "\n";
+		std::cout << Roy << "\n";
+
+		try
+		{
+			Jen.decrement_grade();
+		}
+		catch (Bureaucrat::GradeTooHighException exception)
+		{
+			std::cout << exception.what();
+		}
+		try
+		{
+			Roy.increment_grade();
+		}
+		catch (Bureaucrat::GradeTooLowException exception)
+		{
+			std::cout << exception.what();
+		}
+	}
+	return (0);
+}
