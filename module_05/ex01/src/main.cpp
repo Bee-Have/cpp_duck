d543ed93974225d9500b9b07a018c32ccc28d3ef
Albe MARINI-AUDOUARD <amarini-@e1r9p20.clusters.42paris.fr>
2022-08-28 07:05:04 +0200

    ✅ -Implemented custom tests for the new Form class and its features

--- module_05/ex01/src/main.cpp
+++ module_05/ex01/src/main.cpp
@@ -6,12 +6,13 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 01:57:07 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/28 03:25:52 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/28 06:59:46 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 #define GB "\033[1;32m"
 #define YB "\033[1;33m"
@@ -19,75 +20,50 @@
 
 int	main()
 {
-	std::cout << GB << "Simple tests :\n" << END;
+	std::cout << GB << "Simple working test :\n" << END;
 	{
-		std::cout << YB << "Bureaucrat Moss(10, Moss)\n" << END;
-		std::cout << YB << "Bureaucrat Jen(Moss)\n" << END;
 		Bureaucrat	Moss(10, "Moss");
-		Bureaucrat	Jen(Moss);
+		Form		F0("F0", 10, 10);
 
-		std::cout << Moss << "\n";
-		std::cout << Jen << "\n";
-
-		std::cout << YB << "Grade change test :\n" << END;
-		Moss.decrement_grade();
-		Jen.increment_grade();
-		std::cout << Moss << "\n";
-		std::cout << Jen << "\n";
+		std::cout << YB << "Bureaucrat signs the form :\n" << END;
+		std::cout << Moss << '\n';
+		F0.beSigned(Moss);
+		std::cout << YB << "Here the form is signed twice, it is already signed, nothing happens\n" << END;
+		F0.beSigned(Moss);
 	}
 
-	std::cout << GB << "\nBad Constructor tests :\n" << END;
+	std::cout << GB << "\nError tests with form :\n" << END;
 	{
-		std::cout << YB << "Test with default constructor :\n" << END;
+		std::cout  << YB << "Trying to create a form with bad signing grade :\n" << END;
 		try
 		{
-			Bureaucrat	Moss;
+			Form	bad_form("bad form", 0, 4);
 		}
-		catch (Bureaucrat::GradeTooLowException exception)
+		catch (Form::GradeTooHighException &exception)
 		{
 			std::cout << exception.what();
 		}
-		std::cout << YB << "Test with int Constructor < 1 :\n" << END;
+		std::cout << YB << "Trying to create a form with bad execution grade :\n" << END;
 		try
 		{
-			Bureaucrat	Roy(-19);
+			Form	bad_form("bad form", 140, 151);
 		}
-		catch (Bureaucrat::GradeTooHighException exception)
-		{
-			std::cout << exception.what();
-		}
-		std::cout << YB << "Test with (int, name) Constructor > 150 :\n" << END;
-		try
-		{
-			Bureaucrat Jen(180, "Jen");
-		}
-		catch (Bureaucrat::GradeTooLowException exception)
+		catch (Form::GradeTooLowException &exception)
 		{
 			std::cout << exception.what();
 		}
 	}
 
-	std::cout << GB << "\nTest incrementing/decrementing maximum grades :\n" << END;
+	std::cout << GB << "\nError test with bureaucrat having a grade too low :\n" << END;
 	{
-		Bureaucrat	Jen(1, "Jen");
-		Bureaucrat	Roy(150, "Roy");
-
-		std::cout << Jen << "\n";
-		std::cout << Roy << "\n";
+		Bureaucrat	Jen(42, "Jen");
+		Form		good_form("good form", 40, 40);
 
 		try
 		{
-			Jen.decrement_grade();
+			good_form.beSigned(Jen);
 		}
-		catch (Bureaucrat::GradeTooHighException exception)
-		{
-			std::cout << exception.what();
-		}
-		try
-		{
-			Roy.increment_grade();
-		}
-		catch (Bureaucrat::GradeTooLowException exception)
+		catch (Form::GradeTooLowException &exception)
 		{
 			std::cout << exception.what();
 		}
