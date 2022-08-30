5ca8d569323d69925cedc698874879273bd9165a
amarini- <duck.marini@gmail.com>
2022-08-30 07:18:05 +0200

    ✅ -Added tests for new exception and all forms

--- module_05/ex02/src/main.cpp
+++ module_05/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 01:57:07 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/30 04:48:30 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/30 07:15:11 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,84 @@
 
 int	main(void)
 {
+	std::cout << GB << "Simple working tests :\n" << END;
 	{
 		Bureaucrat	Moss(1, "Moss");
 		Form		*bush = new ShrubberyCreationForm("bush");
 		Form		*robot = new RobotomyRequestForm("robot");
 		Form		*pardon = new PresidentialPardonForm("Arthud Dent");
 		
+		std::cout << YB << "Shrubbery form :\n" << END;
 		bush->beSigned(Moss);
 		bush->execute(Moss);
-		std::cout << "\n";
+		std::cout << YB << "Robotomize form :\n" << END;
 		robot->beSigned(Moss);
 		robot->execute(Moss);
-		std::cout << "\n";
+		std::cout << YB << "Presidential pardon form :\n" << END;
 		pardon->beSigned(Moss);
 		pardon->execute(Moss);
+		
+		delete bush;
+		delete robot;
+		delete pardon;
+	}
+
+	std::cout << GB << "\nBad target in form creating constructor test :\n" << END;
+	{
+		try
+		{
+			ShrubberyCreationForm	test(NULL);
+		}
+		catch (ShrubberyCreationForm::BadTargetInit &exception)
+		{
+			std::cout << exception.what();
+		}
+	}
+
+	std::cout << GB << "\nBad bureaucrat grades for signing/executing forms :\n" << END;
+	{
+		Bureaucrat			Moss(3, "Moss");
+		Bureaucrat			Roy(150, "Roy");
+		RobotomyRequestForm	robot("I am a robot");
+		Form				*robot_pointer;
+		
+		robot_pointer = &robot;
+		
+		std::cout << YB << "Signing form with a low grade :\n" << END;
+		try
+		{
+			robot_pointer->beSigned(Roy);
+		}
+		catch(Form::GradeTooLowException &exception)
+		{
+			std::cout << exception.what();
+		}
+		robot_pointer->beSigned(Moss);
+		std::cout << YB << "Executing form with a low grade :\n" << END;
+		try
+		{
+			robot_pointer->execute(Roy);
+		}
+		catch(Form::GradeTooLowException &exception)
+		{
+			std::cout << exception.what();
+		}
+	}
+
+	std::cout << GB << "\nExecuting a form without signing it :\n" << END;
+	{
+		Bureaucrat				Jen(85, "Jen");
+		PresidentialPardonForm	pardon("sorry(not sorry)");
+		Form					&pardon_ref = pardon;
+		
+		try
+		{
+			pardon_ref.execute(Jen);
+		}
+		catch (Form::FormNotSigned &exception)
+		{
+			std::cout << exception.what();
+		}
 	}
 	return (0);
 }
