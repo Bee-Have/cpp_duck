95e819d985b5b55f2eb0db188ec539fd92368077
amarini- <duck.marini@gmail.com>
2022-08-30 03:03:11 +0200

    ✨ -Implemented all the methods from robotomy class

---
+++ module_05/ex02/src/forms/RobotomyRequestForm.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 01:35:52 by amarini-          #+#    #+#             */
+/*   Updated: 2022/08/30 03:00:53 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctime>
+#include "RobotomyRequestForm.hpp"
+
+/**
+-----------------------------| CONSTRUCTORS |-----------------------------------
+**/
+RobotomyRequestForm::RobotomyRequestForm(void): Form("robotomy", 72, 45), _target("NoTarget")
+{}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm &cpy): Form(cpy), _target(cpy._target)
+{}
+
+RobotomyRequestForm::RobotomyRequestForm(const char *newtarget): Form("robotomy", 75, 45), _target(newtarget)
+{}
+
+/**
+------------------------------| DESTRUCTOR |------------------------------------
+**/
+RobotomyRequestForm::~RobotomyRequestForm(void)
+{}
+
+/**
+--------------------------| ASSIGNMENT OPERATOR |-------------------------------
+**/
+RobotomyRequestForm	&RobotomyRequestForm::operator=(const RobotomyRequestForm &assign)
+{
+	if (_target.compare(assign._target) != 0)
+		_target.assign(assign._target);
+	return (*this);
+}
+
+/**
+--------------------------------| ACCESSORS |-----------------------------------
+**/
+std::string	RobotomyRequestForm::get_target(void) const
+{
+	return (_target);
+}
+
+/**
+--------------------------------| METHODS |-------------------------------------
+**/
+void	RobotomyRequestForm::to_execute(void) const
+{
+	std::cout << "*drill noises*\n";
+	srand(time (0));
+	if ((rand() % 2) == 0)
+		std::cout << _target << " has been Sucessfully robotomized\n" << std::endl;
+	else
+		std::cout << _target << " failed to be robotomized\n" << std::endl;
+}
