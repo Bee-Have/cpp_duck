52341f6a791e047403b302328e26e09bd8557211
amarini- <duck.marini@gmail.com>
2022-08-30 04:49:55 +0200

    ✅✨ -Added simple test in main to test presidential pardon class. Also added according files to the Makefile

--- module_05/ex02/src/main.cpp
+++ module_05/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 01:57:07 by amarini-          #+#    #+#             */
-/*   Updated: 2022/08/30 02:59:53 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/08/30 04:48:30 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "Form.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 #define GB "\033[1;32m"
 #define YB "\033[1;33m"
@@ -26,12 +27,16 @@ int	main(void)
 		Bureaucrat	Moss(1, "Moss");
 		Form		*bush = new ShrubberyCreationForm("bush");
 		Form		*robot = new RobotomyRequestForm("robot");
+		Form		*pardon = new PresidentialPardonForm("Arthud Dent");
 		
 		bush->beSigned(Moss);
 		bush->execute(Moss);
 		std::cout << "\n";
 		robot->beSigned(Moss);
 		robot->execute(Moss);
+		std::cout << "\n";
+		pardon->beSigned(Moss);
+		pardon->execute(Moss);
 	}
 	return (0);
 }
