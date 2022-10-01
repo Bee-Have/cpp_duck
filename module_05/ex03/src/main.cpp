3996f7ba1e96970bb63e8f90d009dc7cb8dba3a9
Albe MARINI-AUDOUARD <amarini-@e1r13p9.clusters.42paris.fr>
2022-10-01 05:37:45 +0200

    🔥 -Deleted main content to write new tests

--- module_05/ex03/src/main.cpp
+++ module_05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 01:57:07 by amarini-          #+#    #+#             */
-/*   Updated: 2022/10/01 02:52:07 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/10/01 04:27:19 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,103 +23,6 @@
 
 int	main(void)
 {
-	std::cout << GB << "Simple working tests :\n" << END;
-	{
-		Bureaucrat	Moss(1, "Moss");
-		Form		*bush = new ShrubberyCreationForm("bush");
-		Form		*robot = new RobotomyRequestForm("robot");
-		Form		*pardon = new PresidentialPardonForm("Arthud Dent");
-		
-		std::cout << YB << "Shrubbery form :\n" << END;
-		bush->beSigned(Moss);
-		bush->execute(Moss);
-		std::cout << YB << "Robotomize form :\n" << END;
-		robot->beSigned(Moss);
-		robot->execute(Moss);
-		std::cout << YB << "Presidential pardon form :\n" << END;
-		pardon->beSigned(Moss);
-		pardon->execute(Moss);
-		
-		delete bush;
-		delete robot;
-		delete pardon;
-	}
-
-	std::cout << GB << "\nBad target in form creating constructor test :\n" << END;
-	{
-		std::cout << YB << "Bad Shrubbery creation form :\n" << END;
-		try
-		{
-			ShrubberyCreationForm	test(NULL);
-		}
-		catch (ShrubberyCreationForm::BadTargetInit &exception)
-		{
-			std::cout << exception.what();
-		}
-		std::cout << YB << "Bad Robotomy creation form :\n" << END;
-		try
-		{
-			RobotomyRequestForm	test(NULL);
-		}
-		catch (RobotomyRequestForm::BadTargetInit &exception)
-		{
-			std::cout << exception.what();
-		}
-		std::cout << YB << "Bad Presidential pardon creation form :\n" << END;
-		try
-		{
-			PresidentialPardonForm	test(NULL);
-		}
-		catch (PresidentialPardonForm::BadTargetInit &exception)
-		{
-			std::cout << exception.what();
-		}
-	}
-
-	std::cout << GB << "\nBad bureaucrat grades for signing/executing forms :\n" << END;
-	{
-		Bureaucrat			Moss(3, "Moss");
-		Bureaucrat			Roy(150, "Roy");
-		RobotomyRequestForm	robot("I am a robot");
-		Form				*robot_pointer;
-		
-		robot_pointer = &robot;
-		
-		std::cout << YB << "Signing form with a low grade :\n" << END;
-		try
-		{
-			robot_pointer->beSigned(Roy);
-		}
-		catch(Form::GradeTooLowException &exception)
-		{
-			std::cout << exception.what();
-		}
-		robot_pointer->beSigned(Moss);
-		std::cout << YB << "Executing form with a low grade :\n" << END;
-		try
-		{
-			robot_pointer->execute(Roy);
-		}
-		catch(Form::GradeTooLowException &exception)
-		{
-			std::cout << exception.what();
-		}
-	}
-
-	std::cout << GB << "\nExecuting a form without signing it :\n" << END;
-	{
-		Bureaucrat				Jen(85, "Jen");
-		PresidentialPardonForm	pardon("sorry(not sorry)");
-		Form					&pardon_ref = pardon;
-		
-		try
-		{
-			pardon_ref.execute(Jen);
-		}
-		catch (Form::FormNotSigned &exception)
-		{
-			std::cout << exception.what();
-		}
-	}
+	
 	return (0);
 }
