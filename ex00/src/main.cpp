456416801690a7e443ea07f9406eb31aba0f6fde
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-19 19:57:45 +0100

    ✨ -Implemented proper parsing for original type of given parameter

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,31 +6,48 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:39:32 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/19 15:59:43 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/19 19:56:57 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
+void	find_type(std::string str)
+{
+	std::cout << "Type : ";
+	if (str.size() == 1 && std::isdigit(str[0]) == 0)
+		std::cout << "char";
+	else if (((str[0] != '-' && str.find_first_not_of("0123456789", 0) == std::string::npos)
+		|| (str[0] == '-' && str.find_first_not_of("0123456789", 1) == std::string::npos))
+		&& ((str[0] != '-' && str.size() < 10) || (str[0] == '-' && str.size() < 11)))
+		std::cout << "int";
+	else if ((str.compare("-inff") == 0 || str.compare("+inff") == 0 || str.compare("nanf") == 0
+		|| (((std::isdigit(str[0]) != 0 && str.find_first_not_of("0123456789.f", 0) == std::string::npos)
+		|| (str[0] == '-' && str.find_first_not_of("0123456789.f", 1) == std::string::npos))
+		&& str.find_first_of(".", 0) == str.find_last_of(".", str.size() -1)
+		&& str.find_first_of("f", 0) == str.find_last_of("f", str.size() -1)))
+		&& str[str.size() - 1] == 'f')
+		std::cout << "float";
+	else if (str.compare("-inf") == 0 || str.compare("+inf") == 0 || str.compare("nan") == 0
+		|| (((std::isdigit(str[0]) != 0 && str.find_first_not_of("0123456789.", 0) == std::string::npos)
+		|| (str[0] == '-' && str.find_first_not_of("0123456789.", 1) == std::string::npos))
+		&& str.find_first_of(".", 0) == str.find_last_of(".", str.size() -1)))
+		std::cout << "double";
+	else
+		std::cout << "undefined";
+	std::cout << std::endl;
+}
+
 int	main(int ac, char **av)
 {
-	(void)ac;
+	if (ac == 1)
+	{
+		std::cout << "No parameter given" << std::endl;
+		return (1);
+	}
 	std::string	convert;
 	convert.assign(av[1]);
-
-	// if size == 1 & str[0] isdigit == 0 : CHAR
-	// if str[last] == f && str[last - 1] isdigit != 0 : FLOAT
-	// if str[last] != f && str has . : DOUBLE
-	// if str[last] != f && str has no . : INT
-	if (convert.size() == 1 && std::isdigit(convert[0]) == 0)
-		std::cout << "It's a character\n";
-	else if (convert[convert.size() - 1] == 'f' && std::isdigit(convert[convert.size() - 2]) != 0)
-		std::cout << "It's a float\n";
-	else if (std::isdigit(convert[convert.size() - 1]) != 0 && convert.find(".", 0) != std::string::npos)
-		std::cout << "It's a double\n";
-	else if (std::isdigit(convert[convert.size() - 1]) != 0 && convert.find(".", 0) == std::string::npos)
-		std::cout << "It's an int\n";
-	
+	find_type(convert);
 	return (0);
 }
