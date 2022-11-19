87952c6455fcd109467ba5d9b7f1cb22d6664d7a
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-19 17:38:19 +0100

    ✨🚧 -Added rudimentary check to identify each type of variable

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:39:32 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/19 11:57:02 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/19 15:59:43 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,22 @@
 
 int	main(int ac, char **av)
 {
-	for (int i = 0; i < ac; ++i)
-		std::cout << "[" << i << "]-[" << av[i] << "]\n";
+	(void)ac;
+	std::string	convert;
+	convert.assign(av[1]);
+
+	// if size == 1 & str[0] isdigit == 0 : CHAR
+	// if str[last] == f && str[last - 1] isdigit != 0 : FLOAT
+	// if str[last] != f && str has . : DOUBLE
+	// if str[last] != f && str has no . : INT
+	if (convert.size() == 1 && std::isdigit(convert[0]) == 0)
+		std::cout << "It's a character\n";
+	else if (convert[convert.size() - 1] == 'f' && std::isdigit(convert[convert.size() - 2]) != 0)
+		std::cout << "It's a float\n";
+	else if (std::isdigit(convert[convert.size() - 1]) != 0 && convert.find(".", 0) != std::string::npos)
+		std::cout << "It's a double\n";
+	else if (std::isdigit(convert[convert.size() - 1]) != 0 && convert.find(".", 0) == std::string::npos)
+		std::cout << "It's an int\n";
+	
 	return (0);
 }
