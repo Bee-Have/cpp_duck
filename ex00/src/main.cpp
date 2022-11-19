3ba320432a36f5439f204205a137129bf4b906b4
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-19 20:12:11 +0100

    ✨ -Made parsing conditions STRONGER :D

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:39:32 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/19 19:56:57 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/19 20:11:28 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,19 @@ void	find_type(std::string str)
 		&& ((str[0] != '-' && str.size() < 10) || (str[0] == '-' && str.size() < 11)))
 		std::cout << "int";
 	else if ((str.compare("-inff") == 0 || str.compare("+inff") == 0 || str.compare("nanf") == 0
-		|| (((std::isdigit(str[0]) != 0 && str.find_first_not_of("0123456789.f", 0) == std::string::npos)
+		|| ((((std::isdigit(str[0]) != 0 || str[0] == '.') && str.find_first_not_of("0123456789.f", 0) == std::string::npos)
 		|| (str[0] == '-' && str.find_first_not_of("0123456789.f", 1) == std::string::npos))
 		&& str.find_first_of(".", 0) == str.find_last_of(".", str.size() -1)
 		&& str.find_first_of("f", 0) == str.find_last_of("f", str.size() -1)))
-		&& str[str.size() - 1] == 'f')
+		&& str[str.size() - 1] == 'f' && str.find(".", 0) != std::string::npos
+		&& str.find_first_of("0123456789", 0) != std::string::npos)
 		std::cout << "float";
 	else if (str.compare("-inf") == 0 || str.compare("+inf") == 0 || str.compare("nan") == 0
-		|| (((std::isdigit(str[0]) != 0 && str.find_first_not_of("0123456789.", 0) == std::string::npos)
+		|| ((((std::isdigit(str[0]) != 0 || str[0] == '.') && str.find_first_not_of("0123456789.", 0) == std::string::npos)
 		|| (str[0] == '-' && str.find_first_not_of("0123456789.", 1) == std::string::npos))
-		&& str.find_first_of(".", 0) == str.find_last_of(".", str.size() -1)))
+		&& str.find_first_of(".", 0) == str.find_last_of(".", str.size() -1)
+		&& str.find(".", 0) != std::string::npos
+		&& str.find_first_of("0123456789", 0) != std::string::npos))
 		std::cout << "double";
 	else
 		std::cout << "undefined";
