5019063c7f03080a8f97a48a67a85890657bf80a
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-22 17:57:17 +0100

    🔒️ -Added throwing error upon trying to create a Form with the default constructor

--- ex03/src/forms/Form.cpp
+++ ex03/src/forms/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 04:12:30 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/17 16:47:40 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/22 17:56:06 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 **/
 Form::Form(void): _name("AForm"), _is_signed(false), _sign_grade(150), _exec_grade(150)
 {
-	// throw excpetion here because no grade has been supplied
+	throw Form::GradeTooHighException();
 }
 
 Form::Form(Form &cpy): _name(cpy._name), _is_signed(cpy._is_signed), _sign_grade(cpy._sign_grade), _exec_grade(cpy._exec_grade)
