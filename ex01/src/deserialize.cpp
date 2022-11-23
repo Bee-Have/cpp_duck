44c997cd5e4e394bd3721438fd2a84a103d3c1d8
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-23 13:49:34 +0100

    🐛 -Fixed serialize|deserialize issues. the casts were not correct and therefore the results were not what i wanted

--- ex01/src/deserialize.cpp
+++ ex01/src/deserialize.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:44:58 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/23 12:47:13 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:48:10 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 Data	*deserialize(uintptr_t raw)
 {
-	Data	*test;
-	test = (Data *)raw;
-	return (test);
+	Data	*reasign_ptr;
+	reasign_ptr = reinterpret_cast<Data *>(raw);
+	return (reasign_ptr);
 }
--- ex01/src/serialize.cpp
+++ ex01/src/serialize.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:28:14 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/23 12:44:54 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/23 13:47:40 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 
 uintptr_t	serialize(Data *ptr)
 {
-	uintptr_t	test;
-	// test = const_cast<uintptr_t>(*tr);
-	test = (uintptr_t)&ptr;
-	return (test);
+	uintptr_t	cpy_ptr;
+	cpy_ptr = reinterpret_cast<uintptr_t>(ptr);
+	return (cpy_ptr);
 }
