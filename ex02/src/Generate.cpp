63831b49d75f5f131bbe96dc273315e4be1da5df
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-25 13:52:40 +0100

    🐛 -Fixed rand() return value. even with time the value was always the same (because time was the same between calls). by adding rand to time the seed becomes more random

--- ex02/src/Generate.cpp
+++ ex02/src/Generate.cpp
@@ -6,9 +6,11 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 15:45:05 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/23 15:57:53 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/25 13:52:16 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 #include <cstdlib>
 #include <ctime>
@@ -19,14 +21,15 @@
 
 Base	*generate(void)
 {
-	int	random_class;
+	int	random_class = rand();
 	Base	*classes[3];
 
 	classes[0] = new A();
 	classes[1] = new B();
 	classes[2] = new C();
 
-	std::srand(std::time(0));
-	random_class = std::rand();
-	return (classes[random_class % 3]);
+	std::srand(std::time(0) + rand());
+	random_class = rand() % 3;
+	std::cout << "random : " << random_class << "\n";
+	return (classes[random_class]);
 }
