7e12c766d5dfed055c2c237e6d9524a6cfda9c8d
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-28 18:15:54 +0100

    ✅ -Added 42 subject tests to the main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,13 +6,27 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:57:30 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/28 15:57:48 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:33:47 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "whatever.hpp"
 
 int	main(void)
 {
-	return (1);
+	// 42 tests
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
