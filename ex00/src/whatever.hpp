8e3e5a3d387dca506d2e16ce3982b80374b1af23
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-28 18:15:06 +0100

    🐛 -Swap was the wrong variables. it had to be references otherwise the values passed were just copies and not the original variables

--- ex00/src/whatever.hpp
+++ ex00/src/whatever.hpp
@@ -6,28 +6,26 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:24:04 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/28 15:58:05 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:29:46 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <string>
-
 // swap template
 // no idea if i'm doing this right...
-template<typename T>
-void	swap(T src, T dst)
+template <typename T>
+void	swap(T &src, T &dst)
 {
 	T	tmp = dst;
-	
+
 	dst = src;
 	src = tmp;
 }
 
 // min template
 // no idea if i'm doing this right...
-template<typename T>
+template <typename T>
 T	min(T src, T cmp)
 {
 	if (src < cmp)
@@ -37,7 +35,7 @@ T	min(T src, T cmp)
 
 // max template
 // no idea if i'm doing this right...
-template<typename T>
+template <typename T>
 T	max(T src, T cmp)
 {
 	if (src > cmp)
