04d9bf72935b74e2a6729db20c79f593fb7c91d9
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-29 15:10:49 +0100

    ✨✅ -Added functions to test in iter.hpp file

--- ex01/src/iter.hpp
+++ ex01/src/iter.hpp
@@ -6,11 +6,13 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 18:53:51 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/29 12:12:14 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/29 14:27:27 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
 
 template <typename T>
 void	To_Upper(T &arg)
@@ -20,8 +22,31 @@ void	To_Upper(T &arg)
 }
 
 template <typename T>
-void	iter(T **addr, int size, void(f(T&)))
+void	To_Lower(T &arg)
+{
+	arg += 32;
+	return ;
+}
+
+template <typename T>
+void	To_Fourty_Two(T &arg)
+{
+	arg = 42;
+	return ;
+}
+
+template <typename T>
+void	print_array(T *array, int size)
 {
 	for (int i = 0; i < size; ++i)
-		f((*addr)[i]);
+		std::cout << "[" << array[i] << "]";
+	std::cout << "\n";
+	return ;
+}
+
+template <typename T>
+void	iter(T *array, int size, void(f(T&)))
+{
+	for (int i = 0; i < size; ++i)
+		f(array[i]);
 }
