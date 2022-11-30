54fa1c8959e871cfb6d791b9d27115ba4534acc6
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-30 16:11:12 +0100

    ✅ -Added more tests to main. Also added colors to it to make it more readable

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,13 +6,18 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:01:48 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/30 15:11:24 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/30 16:10:40 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
 #include "Array.hpp"
+
+#define BOLD "\033[1m"
+#define GB "\033[1;32m"
+#define YB "\033[1;33m"
+#define END "\33[0m"
 
 template<typename T>
 void	print_array(Array<T> array)
@@ -24,39 +29,41 @@ void	print_array(Array<T> array)
 
 int	main(void)
 {
-	// simple working test
+	std::cout << GB << "Simple working tests :\n";
 	{
-		// make int array - get index - get size
+		std::cout << "INT array tests :\n" << END;
 		Array<int>	int_array(3);
 
 		int_array[0] = 42;
 		int_array[1] = 0;
 		int_array[2] = 4183;
+		std::cout << YB << "Print array after initializing it :\n" << END;
 		print_array(int_array);
-		std::cout << "Size of array is : " << int_array.size() << "\n";
+		std::cout << YB << "Size of array is : "  << END << int_array.size() << "\n";
 
-		// make char array - get index - get size
-		Array<char>	char_array(6);
-		char_array[0] = 'k';
+		std::cout << GB << "CHAR array tests :\n" << END;
+		Array<char>	char_array(5);
+		char_array[0] = 'B';
 		char_array[1] = 'e';
-		char_array[2] = 'n';
+		char_array[2] = 'b';
 		char_array[3] = 'o';
-		char_array[4] = 'b';
-		char_array[5] = 'i';
+		char_array[4] = 'p';
+		std::cout << YB << "Print array after initializing it :\n" << END;
 		print_array(char_array);
-		std::cout << "Size of array is : " << char_array.size() << "\n";
+		std::cout << YB << "Size of array is : "  << END << char_array.size() << "\n";
 
-		// make float array - get index - get size
+		std::cout << GB << "FLOAT array tests :\n" << END;
 		Array<float>	float_array(42);
 		for (int i = 0; i < 10; ++i)
 			float_array[i] = 42.42f;
+		std::cout << YB << "Print array after initializing it :\n" << END;
 		print_array(float_array);
-		std::cout << "Size of array is : " << float_array.size() << "\n";
+		std::cout << YB << "Size of array is : "  << END << float_array.size() << "\n";
 	}
-	// tests of default values
+	std::cout << GB << "\nDefault values tests (implied by 42 subject) :\n";
 	{
 		int	size = 3;
-		// exemple with default int values
+		std::cout << "Exemple with classic int array :\n" << END;
 		{
 			int	*int_array = new int[size];
 			std::cout << '[' << int_array[0] << "]";
@@ -65,18 +72,21 @@ int	main(void)
 			std::cout << "\n";
 			delete[] int_array;
 		}
-		// my result with the same test
+		std::cout << YB << "Result with my int array :\n" << END;
 		{
 			Array<int>	my_array(size);
 			print_array(my_array);
 			std::cout << "\n";
 		}
 	}
-	// test exceptions with vector as reference
+	std::cout << GB << "\nReading out of bound exception tests :\n";
 	{
+		std::cout << "Creating vector array of INT\n";
 		Array<int>			my_array(5);
+		std::cout << "Creating my array of INT\n" << END;
 		std::vector<int>	vector_array(5);
 
+		std::cout << YB << "Trying to access element out of bound of array with Vector :\n" << END;
 		try
 		{
 			vector_array.at(10);
@@ -86,6 +96,7 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 
+		std::cout << YB << "Trying to access element out of bound of array with my Array :\n" << END;
 		try
 		{
 			my_array[10];
