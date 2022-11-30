eefdf32500dc092a09bd83ade52e2faaa8efb275
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-11-30 15:15:36 +0100

    ✨🐛🔥 ✨ -Added _size variable to keep track of the size of the array 🐛 -Fixed a lot of issues with everything. Content of Array.cpp was trensfered into Array.hpp because it won't work otherwise (i could use .tpp but it is needlessly complicated so fuck that) 🔥 -Deleted Array.cpp because it is no longer needed actually

--- ex02/src/Array.hpp
+++ ex02/src/Array.hpp
@@ -6,28 +6,67 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 15:45:40 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/29 17:56:02 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/11/30 15:07:54 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <sstream>
+#include <cstddef>
+#include <stdexcept>
+#include <string>
 
 template<typename T>
 class Array
 {
 	public:
 	// Constructors
-		Array();
-		Array(Array &cpy);
-		Array(unsigned int n);
+		Array(): _size(0), _array(NULL) {}
+		Array(Array &cpy): _size(cpy._size), _array(new T[cpy._size])
+		{
+			for (unsigned int i = 0; i < cpy._size; ++i)
+				_array[i] = cpy._array[i];
+		}
+		Array(unsigned int n): _size(n), _array(new T[n]) {}
+
 	// Destructor
-		~Array();
+		~Array()
+		{
+			if (_size != 0)
+				delete[] _array;
+		}
+
 	// Copy assignment operator
-		Array	&operator=(Array &assign);
-	// Methods
-		int	size() const;
+		Array	&operator=(Array &assign)
+		{
+			if (*this != assign)
+			{
+				_size = assign._size;
+				if (_size != 0)
+					delete[] _array;
+				_array = new T[assign.size()];
+				for (int i = 0; i < assign.size(); ++i)
+					_array[i] = assign[i];
+			}
+			return (*this);
+		}
+
+	// Subscript operator
+		T	&operator[](size_t i)
+		{
+			std::ostringstream ss;
+			ss << "Array::operator[]: i (which is " << i
+				<< ") >= this->size() (which is " << _size << ")\n";
+			if (i >= _size)
+				throw std::out_of_range(ss.str());
+			return (_array[i]);
+		}
+
 	// Encapsulation
-		Array	get_array() const;
+		int	size() const {return (_size);}
 	private:
-		T	*_array;
+	// Attributes
+		unsigned int	_size;
+		T				*_array;
 };
