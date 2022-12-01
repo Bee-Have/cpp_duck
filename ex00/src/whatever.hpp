f532778c21bc4a5a2e7ee8f9cc2387be9ea06c1e
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-01 17:27:53 +0100

    💡 -Made better and actually detailed comments of functions in ex00 whatever.hpp

--- ex00/src/whatever.hpp
+++ ex00/src/whatever.hpp
@@ -6,25 +6,27 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:24:04 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/28 17:29:46 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/01 17:26:33 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #pragma once
 
 // swap template
-// no idea if i'm doing this right...
+// Swap twp values of same type T
 template <typename T>
 void	swap(T &src, T &dst)
 {
-	T	tmp = dst;
+	T	tmp(dst);
 
 	dst = src;
 	src = tmp;
 }
 
 // min template
-// no idea if i'm doing this right...
+// Returns tiniest value between same type T variables
+// If values are equal : returns the second one
 template <typename T>
 T	min(T src, T cmp)
 {
@@ -34,7 +36,8 @@ T	min(T src, T cmp)
 }
 
 // max template
-// no idea if i'm doing this right...
+// Returns biggest value between same type T variables
+// If values are equal : returns the second one
 template <typename T>
 T	max(T src, T cmp)
 {
