fb57125c8bbd7281e9fe06d0261d952f5450704e
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-01 14:36:35 +0100

    🚧 -Made easyfind() function, however nothing is working and i don't know how the fuck i'm gonna do that so good luck future me X)

---
+++ ex00/src/easyfind.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 20:25:11 by amarini-          #+#    #+#             */
+/*   Updated: 2022/11/30 20:35:24 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template<typename T>
+int	easyfind(T &type, int value)
+{
+	int	index = 0;
+	for (int i = 0; )
+	// throw out of range if nothing is found
+}
