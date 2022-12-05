809ffdff3735cebd522d60db1a9b1a4b55531633
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-05 16:09:07 +0100

    ✨ -Added the mandatory array, the operator[] as well as a size() function (all present in types of containers)

--- ex01/src/Span.hpp
+++ ex01/src/Span.hpp
@@ -6,9 +6,13 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:53:02 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/05 16:00:00 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/05 16:08:17 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+#include <cstdlib>
 
 class Span
 {
@@ -21,12 +25,17 @@ class Span
 		~Span();
 	// Assignment operator
 		Span	operator=(Span &assign);
+	// Reference operator
+		int		operator[](size_t n) const;
 	// Method
 		void	addRange(int min, int max);
 		void	addNumber(int value);
 		int		shortestSpan() const;
 		int		longestSpan() const;
+	// Accesor
+		int		size() const;
 
 	private:
 		unsigned int	_size;
+		int				_span;
 };
