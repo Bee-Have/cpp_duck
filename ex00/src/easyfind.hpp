ce1982d15c037304db53547c5eeaf7d90c38c406
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-05 14:15:51 +0100

    ✨ -Added two version of easy find. One that returns int and one that returns an iterator.

--- ex00/src/easyfind.hpp
+++ ex00/src/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 20:25:11 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/04 17:31:43 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/05 14:08:55 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,41 @@
 # include <sstream>
 # include <stdexcept>
 
-template<typename T>
-int	easyfind(T &type, int value)
+namespace integer
 {
-	// typename T::iterator	it = type.begin();
-	// int	i = 0;
-	// for (; it != type.back(); ++it && ++i)
-	// 	if (it == value)
-	// 		return (i);
-	for (int i = 0; i < type.size(); ++i)
-		if (type[i] == value)
-			return (i);
+	template<typename T>
+	int	easyfind(T &type, int value)
+	{
+		typename T::iterator	it = type.begin();
+		for (int	i = 0; it != type.end(); ++it, ++i)
+			if (*it == value)
+				return (i);
 
-	std::ostringstream	ss;
+		std::ostringstream	ss;
 
-	ss << "::easyfind: value (which is " << value
-		<<  ") >= <typename T>type->size() (which is " << type.size() << ")";
-	throw std::out_of_range(ss.str());
+		ss << "integer::easyfind: value (which is " << value
+			<<  ") >= <typename T>type->size() (which is " << type.size() << ")";
+		throw std::out_of_range(ss.str());
+	}
 }
+
+namespace iterator
+{
+	template<typename T>
+	typename T::iterator	easyfind(T &type, int value)
+	{
+		typename T::iterator	it = type.begin();
+		for (; it != type.end(); ++it)
+			if (*it == value)
+				return (it);
+
+		std::ostringstream	ss;
+
+		ss << "iterator::easyfind: value (which is " << value
+			<<  ") >= <typename T>type->size() (which is " << type.size() << ")";
+		throw std::out_of_range(ss.str());
+	}
+}
+
 
 #endif
