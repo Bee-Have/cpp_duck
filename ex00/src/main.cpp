0ffab941966552e4fa470dea3a4df38f3e3f1726
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-05 15:34:21 +0100

    ✨✅ -Added printing function as well as further prints to better understand tests and what they entail

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 20:30:03 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/05 14:10:04 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:34:09 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,32 @@
 #include <iostream>
 #include "easyfind.hpp"
 
+
+template <typename T>
+void	print_array(T &array)
+{
+	typename T::iterator	it = array.begin();
+
+	for (; it != array.end(); ++it)
+		std::cout << '[' << *it << ']';
+	std::cout << std::endl;
+}
+template <>
+void	print_array(std::list<int> &array)
+{
+	std::list<int>::iterator	it = array.begin();
+
+	for (; it != array.end(); ++it)
+		std::cout << '[' << static_cast<char>(*it) << ']';
+	std::cout << std::endl;
+}
+
 int	main(void)
 {
 	// test with vector
-	std::cout << "\n";
+	std::cout << "Simple tests with container \'vector\' :\n";
 	{
-		std::vector<int>	vector_int(7);
+		std::vector<int>	vector_int;
 
 		// fill vector
 		vector_int.push_back(4183);
@@ -33,10 +53,14 @@ int	main(void)
 		vector_int.push_back(-130);
 		vector_int.push_back(4183);
 
+		print_array(vector_int);
+
 		// working simple test
-		integer::easyfind(vector_int, -130);
+		std::cout << "position of \"-130\" is " << integer::easyfind(vector_int, -130) << '\n';
+
 		// working test mutiple occurences
-		integer::easyfind(vector_int, 4183);
+		std::cout << "position of \"4183\" is " << integer::easyfind(vector_int, 4183) << '\n';
+
 		// try catch with non working test
 		try
 		{
@@ -47,10 +71,11 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 	}
+
 	// test with deque
-	std::cout << "\n";
+	std::cout << "Simple tests with container \'deque\' :\n";
 	{
-		std::deque<int>	deque_int(10);
+		std::deque<int>	deque_int;
 
 		// fill deque
 		deque_int.push_back(1);
@@ -63,11 +88,17 @@ int	main(void)
 		deque_int.push_back(1);
 		deque_int.push_back(42);
 		deque_int.push_back(-130);
-		
+
+		print_array(deque_int);
+
 		// working simple test
-		iterator::easyfind(deque_int, 0);
+		std::cout << "position of \"10\" is "
+			<< iterator::easyfind(deque_int, 10) - deque_int.begin() << '\n';
+			// << std::distance(deque_int.begin(), iterator::easyfind(deque_int, 10)) << '\n';
+	
 		// working test mutiple occurences
-		iterator::easyfind(deque_int, 1);
+		std::cout << "position of \"1\" is "
+			<< iterator::easyfind(deque_int, 1) - deque_int.begin() << '\n';
 		// try catch with non working test
 		try
 		{
@@ -78,10 +109,11 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 	}
+
 	// test with list
-	std::cout << "\n";
+	std::cout << "Simple tests with container \'list\' :\n";
 	{
-		std::list<int>	list_int(12);
+		std::list<int>	list_int;
 
 		// fill list
 		list_int.push_back('a');
@@ -97,14 +129,18 @@ int	main(void)
 		list_int.push_back('.');
 		list_int.push_back('*');
 
+		print_array(list_int);
+
 		// working simple test
-		integer::easyfind(list_int, 'a');
+		std::cout << "position of \"a\" is " << integer::easyfind(list_int, 'a') << '\n';
 		// working test mutiple occurences
-		iterator::easyfind(list_int, '*');
+		std::cout << "position of \"*\" is "
+			<< std::distance(list_int.begin(), iterator::easyfind(list_int, '*')) << '\n';
+			// << iterator::easyfind(list_int, '*') - list_int.begin() << '\n';
 		// try catch with non working test
 		try
 		{
-			integer::easyfind(list_int, 'b');
+			iterator::easyfind(list_int, 'b');
 		}
 		catch(const std::exception& e)
 		{
