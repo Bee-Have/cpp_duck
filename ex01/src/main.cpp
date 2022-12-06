a3e0f1635fd4239f08cc60751bcd30cd5f038fb1
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-06 13:59:14 +0100

    ✅ -Changed all calls to print_array() into calls with iter with specific function to print every element

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 12:00:04 by amarini-          #+#    #+#             */
-/*   Updated: 2022/11/29 15:09:45 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/06 13:58:43 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,44 +26,44 @@ int	main(void)
 		int	int_array[] = {0, 1, 2, 3, 4};
 
 		std::cout << BOLD<< "Original INT array :\n" << END;
-		print_array(int_array, 5);
+		iter(int_array, 5, print);
 		
-		std::cout << YB <<  "Call to iter() with To_Lower() (+32 to each element)\n" << END;
+		std::cout << YB <<  "\nCall to iter() with To_Lower() (+32 to each element)\n" << END;
 		iter(int_array, 5, To_Lower);
 		
 		std::cout << BOLD << "Array after To_Lower() :\n" << END;
-		print_array(int_array, 5);
+		iter(int_array, 5, print);
 		
-		std::cout << YB << "Call to iter() with To_Fourty_Two()(assign every element to 42)\n" << END;
+		std::cout << YB << "\nCall to iter() with To_Fourty_Two()(assign every element to 42)\n" << END;
 		iter(int_array, 5, To_Fourty_Two);
 		
 		std::cout << BOLD << "Array after To_Fourty_Two() :\n" << END;
-		print_array(int_array, 5);
+		iter(int_array, 5, print);
 	}
-	std::cout << GB << "\nSimple CHAR* tests :\n" << END;
+	std::cout << GB << "\n\nSimple CHAR* tests :\n" << END;
 	{
 		char	str[] = "general.kenobi";
 
 		std::cout << BOLD << "Original CHAR* array :" << END << std::endl;
-		print_array(str, std::strlen(str));
+		iter(str, std::strlen(str), print);
 
-		std::cout << YB << "Call to iter() with To_Upper() (-32 to each element)\n" << END;
+		std::cout << YB << "\nCall to iter() with To_Upper() (-32 to each element)\n" << END;
 		iter(str, std::strlen(str), To_Upper);
 
 		std::cout << BOLD << "Array after To_Upper() :" << END << std::endl;
-		print_array(str, std::strlen(str));
+		iter(str, std::strlen(str), print);
 
-		std::cout << YB << "Call to iter() with To_Lower() (+32 to each element)\n" << END;
+		std::cout << YB << "\nCall to iter() with To_Lower() (+32 to each element)\n" << END;
 		iter(str, std::strlen(str), To_Lower);
 
 		std::cout << BOLD << "Array after To_Lower() :" << END << std::endl;
-		print_array(str, std::strlen(str));
+		iter(str, std::strlen(str), print);
 
-		std::cout << YB << "Call to iter() with To_Fourty_Two()(assign every element to 42)\n" << END;
+		std::cout << YB << "\nCall to iter() with To_Fourty_Two()(assign every element to 42)\n" << END;
 		iter(str, std::strlen(str), To_Fourty_Two);
 
 		std::cout << BOLD << "Array after To_Fourty_Two() :" << END << std::endl;
-		print_array(str, std::strlen(str));
+		iter(str, std::strlen(str), print);
 	}
 	return (0);
 }
