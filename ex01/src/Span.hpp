5b0025b051bcc77905b71aeef87401874acb7aa3
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-06 13:26:49 +0100

    ✨ -Added libraries to Span.hpp to utilize more functions in Span.cpp

--- ex01/src/Span.hpp
+++ ex01/src/Span.hpp
@@ -6,17 +6,17 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:53:02 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/05 17:00:47 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/06 13:20:29 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <sstream>
-#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <cstdlib>
+#include <limits>
 
 class Span
 {
@@ -31,6 +31,7 @@ class Span
 	// Reference operator
 		int		operator[](size_t n) const;
 	// Method
+		void	fill(int value);
 		void	addRange(int min, int max);
 		void	addNumber(int value);
 		int		shortestSpan() const;
@@ -40,5 +41,6 @@ class Span
 
 	private:
 		unsigned int	_size;
+		int				_pos_max;
 		int				*_span;
 };
