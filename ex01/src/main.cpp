d9688f3d1ced12aab8cc69994cf49f27785e641b
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-07 13:03:32 +0100

    ✅ -Polished tests and added colors

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,20 +6,25 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:20:53 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/07 12:52:50 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/07 13:01:26 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+#define BOLD "\033[1m"
+#define GB "\033[1;32m"
+#define YB "\033[1;33m"
+#define END "\33[0m"
+
 int	main(void)
 {
 	// simple test
-	std::cout << "Simple tests :\n";
+	std::cout << GB << "Simple tests :\n";
 	{
 		Span	simple(10);
 
-		std::cout << "Adding numbers repeatedly with addNumber()\n";
+		std::cout << YB << "Adding numbers repeatedly with addNumber()\n" << END;
 		simple.addNumber(1);
 		simple.addNumber(2);
 		simple.addNumber(3);
@@ -33,55 +38,63 @@ int	main(void)
 
 		simple.printSpan();
 
-		std::cout << "Shortest span in _span variable : " << simple.shortestSpan() << '\n';
-		std::cout << "Biggest span in _span variable : " << simple.longestSpan() << '\n';
+		std::cout << BOLD << "Shortest span in _span variable : " << END
+			<< simple.shortestSpan() << '\n';
+		std::cout << BOLD << "Biggest span in _span variable : " << END
+			<< simple.longestSpan() << '\n';
 	}
 
 	// addRange tests
-	std::cout << "\nRange tests :\n";
+	std::cout << GB << "\nRange tests :\n";
 	{
 		Span	simple_range(4);
-		std::cout << "Adding range : [0 - 100] to span of 4 elements :\n";
+		std::cout << YB << "Adding range : [0 - 100] to span of 4 elements :\n" << END;
 		simple_range.addRange(0, 100);
 		simple_range.printSpan();
 		
 		Span	short_range(10);
-		std::cout << "\nAdding range : [10 - 15] to span of 10 elements :\n";
+		std::cout << YB << "\nAdding range : [10 - 15] to span of 10 elements :\n" << END;
 		short_range.addRange(10, 15);
 		short_range.printSpan();
 		
 		Span	big_range(100);
-		std::cout << "\nAdding range : [42 - 4183] to span of 100 elements :\n";
+		std::cout << YB << "\nAdding range : [42 - 4183] to span of 100 elements :\n" << END;
 		big_range.addRange(42, 4183);
 		big_range.printSpan();
 
 		Span	no_range(15);
-		std::cout << "\nAdding range : [42 - 42] to span of 15 elements :\n";
+		std::cout << YB << "\nAdding range : [42 - 42] to span of 15 elements :\n" << END;
 		no_range.addRange(42, 42);
 		no_range.printSpan();
 	}
 
 	// range test
-	std::cout << "\nshortestSpan() longestSpan() tests :\n";
+	std::cout << GB << "\nshortestSpan() longestSpan() tests :\n" << END;
 	{
+		std::cout << YB << "Using span with one value (80):\n" << END;
 		Span	no_diff(10);
 		no_diff.fill(80);
 		no_diff.printSpan();
-		std::cout << "Shortest span in _span variable : " << no_diff.shortestSpan() << '\n';
-		std::cout << "Biggest span in _span variable : " << no_diff.longestSpan() << '\n';
+		std::cout << BOLD << "Shortest span in _span variable : " << END
+			<< no_diff.shortestSpan() << '\n';
+		std::cout << BOLD << "Biggest span in _span variable : " << END
+			<< no_diff.longestSpan() << '\n';
 
+		std::cout << YB << "\nUsing span with two values (1 - 100):\n" << END;
 		Span	short_span(2);
 		short_span.addNumber(1);
 		short_span.addNumber(100);
 		short_span.printSpan();
-		std::cout << "Shortest span in _span variable : " << short_span.shortestSpan() << '\n';
-		std::cout << "Biggest span in _span variable : " << short_span.longestSpan() << '\n';
+		std::cout << BOLD << "Shortest span in _span variable : " << END
+			<< short_span.shortestSpan() << '\n';
+		std::cout << BOLD << "Biggest span in _span variable : " << END
+			<< short_span.longestSpan() << '\n';
 	}
 
 	// error tests
-	std::cout << "\nException tests :\n";
+	std::cout << GB << "\nException tests :\n";
 	{
-		std::cout << "addNumber() exception :\n";
+		std::cout << YB << "addNumber() exception :\n" << END;
 		Span	add_exception;
 		try
 		{
@@ -92,7 +105,7 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 
-		std::cout << "operator[] exception :\n";
+		std::cout << YB << "operator[] exception :\n" << END;
 		Span	access_exception(3);
 		try
 		{
@@ -103,7 +116,7 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 
-		std::cout << "shortestSpan() exception :\n";
+		std::cout << YB << "shortestSpan() exception :\n" << END;
 		Span	tiny_exception(1000);
 		tiny_exception.addNumber(9);
 		try
@@ -114,7 +127,7 @@ int	main(void)
 		{
 			std::cerr << e.what() << '\n';
 		}
-		std::cout << "shortestSpan() exception :\n";
+		std::cout << YB << "shortestSpan() exception :\n" << END;
 		try
 		{
 			tiny_exception.longestSpan();
@@ -124,7 +137,7 @@ int	main(void)
 			std::cerr << e.what() << '\n';
 		}
 
-		std::cout << "addRange() exceptions :\n";
+		std::cout << YB << "addRange() exceptions :\n" << END;
 		Span	range_lenth_exception(2);
 		Span	range_argument_exception(10);
 		range_lenth_exception.addNumber(19);
