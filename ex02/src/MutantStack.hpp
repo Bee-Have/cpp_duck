6aaa7e44ac0934e92e6df66c9857fc04de7e7f84
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2022-12-15 16:34:43 +0100

    ✨ -Implemented fully functioning iterator to the MutantStack class

--- ex02/src/MutantStack.hpp
+++ ex02/src/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 13:46:37 by amarini-          #+#    #+#             */
-/*   Updated: 2022/12/15 16:22:30 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/12/15 16:34:11 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ class MutantStack : public std::stack<T>
 {
 	public:
 	// Iterator :declare iterator here ?
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 
 	// Constructors
 		MutantStack(void): std::stack<T>() {}
@@ -75,6 +77,14 @@ class MutantStack : public std::stack<T>
 		T	top(void) {return (this->c.back());}
 
 	// Iterator methods : begin, end, rbegin, rend
+	iterator			begin(void) {return (this->c.begin());}
+	iterator			begin(void) const {return (this->c.begin());}
+	iterator			end(void) {return (this->c.end());}
+	iterator			end(void) const {return (this->c.end());}
+	reverse_iterator	rbegin() {return (this->c.rbegin());}
+	reverse_iterator	rbegin() const {return (this->c.rbegin());}
+	reverse_iterator	rend() {return (this->c.rend());}
+	reverse_iterator	rend() const {return (this->c.rend());}
 
 	// Capacity
 		MutantStack	empty(void) const {this->c.empty;}
