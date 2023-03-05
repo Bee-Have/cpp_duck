23c2fd90437e64ded1bf49ae532642c6f4d7a0b6
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-05 18:33:29 +0100

    ✨ -ex00: Added first parsing of input_file : checks if no input file, if multiple args, if the file can be opened

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <map>
 
 std::map<std::string, float>	setup_database(void);
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::map<std::string, float> db = setup_database();
+	std::map<std::string, float>	db = setup_database();
+	std::ifstream					input_file;
+
+	if (ac < 2)
+		std::cerr << "Error: no input file\n";
+	if (ac > 2)
+		std::cerr << "Error: multiple arguments\n";
+	input_file.open(av[1], std::ifstream::in);
+	if (input_file.is_open() == false)
+		std::cerr << "Error: could not open input file\n";
 	return (0);
 }
