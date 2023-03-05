c3b8c0bf38d196295f3a7f7db288aed8515605a9
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-05 19:42:29 +0100

    ✨ -ex00: added first version of working program

--- ex00/srcs/input_handling.cpp
+++ ex00/srcs/input_handling.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <map>
 
@@ -18,10 +19,30 @@ std::string	get_line(std::ifstream &input_file)
 	return (line);
 }
 
+float	get_value(std::string line)
+{
+	float				ret = -1;
+	std::string			tmp;
+	std::stringstream	ss;
+
+	tmp = line.substr(line.find('|', 0) + 2, line.size());
+	if (tmp.find_first_not_of("0123456789.", 0) != std::string::npos)
+		std::cerr << "Error: value must be a NUMBER between 0 and 1000\n";
+	else
+	{
+		ss << tmp;
+		ss >> ret;
+		if (ret > 1000)
+			std::cerr << "Error: value must be a NUMBER between 0 and 1000\n";
+	}
+	return (ret);
+}
+
 void	input_handling(std::ifstream &input_file, std::map<std::string, float>db)
 {
 	std::string	line;
 	std::string	date;
+	float		nbr;
 
 	(void)db;
 	line = get_line(input_file);
@@ -33,7 +54,12 @@ void	input_handling(std::ifstream &input_file, std::map<std::string, float>db)
 			date = line.substr(0, line.find_first_of(' '));
 			if (db.find(date) != db.end())
 			{
-				std::cout << "found :" << date << '\n';
+				nbr = get_value(line);
+				if (nbr >= 0 && nbr <= 1000)
+				{
+					std::cout << date << " => " << nbr << " = ";
+					std::cout << (nbr * db.find(date)->second) << '\n';
+				}
 			}
 			else
 				std::cout << "Error: could not find " << date << " in database\n";
