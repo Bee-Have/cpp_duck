15d6cdbf9b05911fe2f0c2843f2b2b518f0db819
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-06 16:16:57 +0100

    🐛 -ex01: i was parsing the string "equation" before setting it so no errors were registered. It's now set before checking :D

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -11,12 +11,13 @@ int	main(int ac, char **av)
 		std::cerr << "Error: no equation given\n";
 	else if (ac > 2)
 		std::cerr << "Error: can only give ONE equation at a time\n";
-	else if (equation.find_first_not_of("0123456789+-/*", 0) != std::string::npos)
-		std::cerr << "Error: equation must ONLY contain NUMBERS and \"+-/*\"\n";
 	else
 	{
 		equation.append(av[1]);
-		rpn(equation);
+		if (equation.find_first_not_of("0123456789+-/* ", 0) != std::string::npos)
+			std::cerr << "Error: equation must ONLY contain NUMBERS and \"+-/*\"\n";
+		else
+			rpn(equation);
 	}
 	return (0);
 }
