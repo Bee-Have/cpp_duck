69adb425d67be73c8dbacc897d02933e8f8458a2
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-06 16:15:43 +0100

    🐛 -ex01: the stringstream was not deleting the old input so i was always adding the first number. stringstream is now declared in the while statement and therefore flushed every round

--- ex01/srcs/rpn.cpp
+++ ex01/srcs/rpn.cpp
@@ -15,16 +15,12 @@ int	calculate(int first, int second, char sign)
 		ret = first / second;
 	else if (sign == '*')
 		ret = first * second;
-	std::cout << "first = " << first << " | second = " << second << '\n';
-	std::cout << "sign = " << sign << '\n';
-	std::cout << "ret = " << ret << '\n';
 	return (ret);
 }
 
 void	rpn(std::string equation)
 {
 	std::stack<int>		container;
-	std::stringstream	ss;
 	char				tmp;
 	int					top;
 	int					second;
@@ -36,15 +32,16 @@ void	rpn(std::string equation)
 		{
 			if (tmp != '+' && tmp != '-' && tmp != '/' && tmp != '*')
 			{
+				std::stringstream	ss;
 				ss << tmp;
 				ss >> top;
 				container.push(top);
 			}
 			else
 			{
-				top = container.top();
-				container.pop();
 				second = container.top();
+				container.pop();
+				top = container.top();
 				container.pop();
 				container.push(calculate(top, second, tmp));
 			}
