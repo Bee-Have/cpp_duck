acb52463dd56cb244c53a18c0af8b86ab16c7613
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-08 14:34:20 +0100

    🔥✨ ex02 🔥 -ex02: deleted old file from Makefile ✨ -ex02: added more parsing to the arguments in main (that way i don't have to do it later and potentially twice)

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
-int	fill_vector(char **av);
+int	parsing(char **av)
+{
+	std::string	check;
+
+	for (int i = 1; av[i] != NULL; ++i)
+	{
+		check.append(av[i]);
+		if (check.find_first_not_of("0123456789 ") != std::string::npos)
+		{
+			std::cerr << "Error: inputs must only be POSITIVE INTEGERS\n";
+			return (1);
+		}
+		check.clear();
+	}
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
 	if (ac < 3)
 		std::cerr << "Error: not enough arguments\n";
+	if (parsing(av) == 0)
+		return (0);
+		// rest of the behavior here
 	return (0);
 }
