2830c344e89c05109a51d859ba78f6a02825b0d0
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-03-17 17:16:17 +0100

    ✨🐛 -ex00: implemented class for database.

--- ex00/srcs/database.cpp
+++ ex00/srcs/database.cpp
@@ -3,16 +3,44 @@
 #include <string>
 #include <map>
 #include <utility>
+#include "Date.hpp"
 
-std::pair<std::string, float>	create_pair(std::string line)
+Date	create_date(std::string date)
 {
-	std::string						date;
-	std::stringstream				ss;
-	int								start;
-	int								end;
-	float							value;
+	Date				new_date;
+	std::string			tmp;
+	std::stringstream	ss;
+	int					value;
 
-	date = line.substr(0, line.find_first_of(','));
+	tmp.assign(date.substr(0, date.find('-')));
+	ss << tmp;
+	ss >> value;
+	new_date.set_year(value);
+	ss.clear();
+
+	date.assign(date.substr(date.find('-') + 1, date.size()));
+	tmp.assign(date.substr(0, date.find('-')));
+	ss << tmp;
+	ss >> value;
+	new_date.set_month(value);
+	ss.clear();
+
+	date.assign(date.substr(date.find('-') + 1, date.size()));
+	ss << date;
+	ss >> value;
+	new_date.set_day(value);
+	return (new_date);
+}
+
+std::pair<Date, float>	create_pair(std::string line)
+{
+	Date				date;
+	std::stringstream	ss;
+	int					start;
+	int					end;
+	float				value;
+
+	date = create_date(line.substr(0, line.find_first_of(',')));
 
 	start = line.find_first_of(',') + 1;
 	end = line.find(',', start) - start;
@@ -20,14 +48,14 @@ std::pair<std::string, float>	create_pair(std::string line)
 	ss << line.substr(start, end);
 	ss >> value;
 
-	return (make_pair(date, value));
+	return (std::make_pair(date, value));
 }
 
-std::map<std::string, float>	setup_database(void)
+std::map<Date, float>	setup_database(void)
 {
-	std::map<std::string, float>	db;
-	std::ifstream					db_file("./srcs/data.csv");
-	std::string						line;
+	std::map<Date, float>	db;
+	std::ifstream			db_file("./srcs/data.csv");
+	std::string				line;
 
 	if (db_file.is_open())
 	{
